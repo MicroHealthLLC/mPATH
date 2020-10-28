9a4dae1b851c88f61675f02c0159b72e363f08e4
bilel-kedidi <bilel.kedidi@gmail.com>
2020-10-28 13:03:19 +0500

    Updated seeds file to load privilege for superadmin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
+puts "Seed the application..."
 
-# if Rails.env.development?
-  User.create_with(
-    password: 'password',
-    password_confirmation: 'password',
-    first_name: 'admin',
-    role: "superadmin",
-    last_name: 'admin'
-  ).find_or_create_by(email: 'admin@example.com')
+user = User.find_or_initialize_by(email: 'admin@example.com')
+user.assign_attributes(
+  password: 'adminPa$$w0rd',
+  password_confirmation: 'adminPa$$w0rd',
+  title: 'Mr.',
+  first_name: 'Super',
+  role: "superadmin",
+  last_name: 'Admin'
+)
+user.privilege = Privilege.new(overview: "RWD", tasks: "RWD", notes: "RWD", issues: "RWD", admin: "RWD", map_view: "RWD", gantt_view: "RWD", watch_view: "RWD", documents: "RWD", members: "RWD")
+user.save(validate: false)
 
-  @region = FacilityGroup.find_or_create_by(name: 'Sample Region', code: 'SAMP')
-  states = [ ]
-  states.each do |state|
-    State.create_with(
-      center: state["center"],
-      region: @region
-    ).find_or_create_by(
-      name: state["name"], code: state["code"]
-    )
-    puts state["name"] + " created!\n"
-  end
+puts "superuser created with email: #{user.email}"
 
-  setting = Setting.first_or_create(google_map_key: ENV['GOOGLE_MAP_KEY'])
-# end
+FacilityGroup.find_or_create_by(name: 'Sample Region', code: 'SAMP', status: 'active')
+Setting.first_or_create(google_map_key: ENV['GOOGLE_MAP_KEY'])
+
+puts "Seeds completed.."
