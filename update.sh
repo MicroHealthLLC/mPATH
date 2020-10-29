a885fb39ac32643eb6287410899471ed9c217843
bluenevus <frank.tucker@microhealthllc.com>
2020-10-28 20:13:49 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,10 +3,10 @@ echo "Deploying mGis"
 
 cd /var/www/mGis \
 && sudo git pull \
-&& sudo bundle install \
-&& sudo rake db:migrate \
-&& sudo yarn install \
-&& sudo rake assets:precompile \
+&& bundle install \
+&& rake db:migrate \
+&& yarn install \
+&& rake assets:precompile \
 && sudo chown -R nginx:nginx * \
 && sudo systemctl restart nginx \
-&& sudo echo "mGis deployed successfully"
+&& echo "mGis deployed successfully"
