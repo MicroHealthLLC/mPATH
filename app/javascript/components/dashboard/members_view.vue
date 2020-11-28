ddf820eb99a1e1ef95f4bbd10005affb2d898991
root <root@HP-ENVY-188.localdomain>
2020-11-28 18:47:59 -0500

    Added sortability feature to Team table.  Still needs pagination.

--- app/javascript/components/dashboard/members_view.vue
+++ app/javascript/components/dashboard/members_view.vue
@@ -14,33 +14,47 @@
         id="printBtn"
         style="font-size:.70rem"
       >Export to Excel</button>
-      <div class="table-responsive-md">
-        <table class="table teamTable table-sm table-bordered mt-2" ref="table" id="teamMembersList">
-          <colgroup>
-            <col class="five" />
-            <col class="twenty" />
-            <col class="fifteen" />
-            <col class="twenty" />
-            <col class="twenty-five" />
-            <col class="fifteen" />
-          </colgroup>
-          <tr class="tableHead">
-            <th></th>
-            <th>Name</th>
-            <th>Position</th>
-            <th>Organization</th>
-            <th>Email</th>
-            <th>Phone</th>
+      <div class="mt-4">   
+        <sorted-table :values="values" ref="table" id="teamMembersList">
+         <thead>
+          <tr class="tableHead"> 
+            <th scope="col" style="text-align: left; width: 4rem;">
+            <sort-link name="id">#</sort-link>
+            </th>                  
+            <th scope="col" style="text-align: left; width: 10rem;">
+            <sort-link name="firstName">First Name</sort-link>
+            </th>
+             <th scope="col" style="text-align: left; width: 10rem;">
+            <sort-link name="lastName">Last Name</sort-link>
+            </th>
+            <th scope="col" style="text-align: left; width: 8rem;">
+            <sort-link name="title">Position</sort-link>
+            </th>
+            <th scope="col" style="text-align: left; width: 11rem;">
+            <sort-link name="organization">Organization</sort-link>
+            </th>
+            <th scope="col" style="text-align: left; width: 15rem;">
+            <sort-link name="email">Email</sort-link>
+            </th>
+             <th scope="col" style="text-align: left; width: 10rem;">
+            <sort-link name="phoneNumber">Phone Number</sort-link>
+            </th>
           </tr>
-          <tr v-for="(user, i) in orderedUsers"  v-if="(user.status) === 'active'" :load="log(user)">
-            <td class="text-center">{{i+1}}</td>
-            <td>{{user.fullName}}</td>
-            <td>{{user.title}}</td>
-            <td>{{user.organization}}</td>
-            <td><a :href="`mailto:${user.email}`">{{user.email}}</a></td>
-            <td><a :href="`tel:${user.phoneNumber}`">{{user.phoneNumber}}</a></td>
+          </thead>
+          <template #body="sort">
+          <tbody>            
+          <tr v-for="value in sort.values" :key="value.id" class="teamTable"> 
+            <td>{{ value.id }}</td>          
+            <td>{{ value.firstName }}</td>
+            <td>{{ value.lastName }}</td>
+            <td>{{ value.title }}</td>
+            <td>{{ value.organization }}</td>
+            <td><a :href="`mailto:${value.email}`">{{ value.email }}</a></td>
+            <td><a :href="`tel:${value.phoneNumber}`">{{ value.phoneNumber }}</a></td>
           </tr>
-        </table>
+          </tbody>
+          </template>
+        </sorted-table>
       </div>
     </div>
   </div>
@@ -48,13 +62,19 @@
 
 <script>
   import {mapGetters, mapActions} from 'vuex'
+  import SortedTablePlugin from "vue-sorted-table";
   import {jsPDF} from "jspdf"
   import 'jspdf-autotable'
+
+Vue.use(SortedTablePlugin, {
+  ascIcon: '<i class="material-icons">arrow_drop_up</i>',
+  descIcon: '<i class="material-icons">arrow_drop_down</i>'
+});
 
   export default {
     name: "TeamMembersView",
     data() {
-      return {
+      return {   
         uri :'data:application/vnd.ms-excel;base64,',
         template:'<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="https://www.w3.org/TR/2018/SPSD-html401-20180327/"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>',
         base64: (s) => window.btoa(unescape(encodeURIComponent(s))),
@@ -62,16 +82,13 @@
       }
     },
     methods: {
-      //download method for PDF
-      //tableToExcel method for excel
+      //download() method for PDF
+      //tableToExcel() method for excel
       download() {
         const doc = new jsPDF("l")
         const html = this.$refs.table.innerHTML
         doc.autoTable({ html: '#teamMembersList' })
         doc.save("Team_Members_list.pdf")
-      },
-      log(user) {
-        console.log(user)
       },
       tableToExcel(table, name) {
         if (!table.nodeType) table = this.$refs.table
@@ -83,38 +100,37 @@
       ...mapGetters([
         'projectUsers'
       ]),
-      orderedUsers() {
-        return _.orderBy(this.projectUsers, 'lastName', 'asc')
-      }
+      values() {
+        return this.projectUsers
+      } 
     }
   }
 </script>
 
 <style scoped lang="scss">
-  table {
-    table-layout: fixed ;
-    width: 100% ;
+ table {
+    table-layout: fixed ;   
     margin-bottom: 0 !important;
-  }
-  .teamTable {
     box-shadow: 0 2.5px 5px rgba(56,56, 56,0.19), 0 3px 3px rgba(56,56,56,0.23);
   }
-  .tableHead {
+  thead {
     background-color: #ededed;
   }
-  .five {
-    width: 5%;
-  }
-  .fifteen {
-    width: 15%;
-  }
-  .twenty {
-    width: 20%;
-  }
-  .twenty-five {
-    width: 25%;
-  }
   td {
-    overflow-wrap: break-word !important;
+    overflow-wrap: break-word;
+   
   }
+  .teamTable:hover {
+   cursor: default;
+    background-color: rgba(91, 192, 222, 0.3);
+    border-left: solid rgb(91, 192, 222);
+  }
+
+  a:visited, a:active, a:hover {
+    text-decoration-line: none !important;    
+  }
+  th > a {
+    color: #383838 !important;
+  }
+  
 </style>
