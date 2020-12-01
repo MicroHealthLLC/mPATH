364c63a60c6d7a671432f2c2f0d0ccdf3bbc925c
Amand Singh <amand.singh@microhealthllc.com>
2020-12-01 15:28:10 -0500

    improve peformance for facility group

--- app/controllers/api/facility_groups_controller.rb
+++ app/controllers/api/facility_groups_controller.rb
@@ -1,6 +1,23 @@
 class Api::FacilityGroupsController < AuthenticatedController
   def index
-    collection = params[:project_id].present? ? FacilityGroup.joins(:facility_projects).where("facility_projects.project_id = ?", params[:project_id]).distinct : FacilityGroup.all
+    tasks_hash = {
+      tasks: [{task_files_attachments: :blob}, :task_type, :users, :task_stage, :checklists, :notes, :related_tasks, :related_issues, :sub_tasks, :sub_issues, {facility_project: :facility} ]
+    }
+    issues_hash = {
+      issues: [{issue_files_attachments: :blob}, :issue_type, :users, :issue_stage, :checklists, :notes, :related_tasks, :related_issues, :sub_tasks, :sub_issues, {facility_project: :facility}, :issue_severity ]
+    }
+    notes_hash = {
+      notes: [{note_files_attachments: :blob}, :user]
+    }
+    include_hash = {
+      facility_projects: [:facility, tasks_hash, issues_hash, notes_hash]
+    }
+
+    if params[:project_id].present?
+      collection = FacilityGroup.includes(include_hash).joins(:facility_projects).where("facility_projects.project_id = ?", params[:project_id]).distinct
+    else
+      collection = FacilityGroup.includes(include_hash).all
+    end
 
     render json: {facility_groups: collection.as_json}
   end
