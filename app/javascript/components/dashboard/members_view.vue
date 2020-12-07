e9f169150df82785c23b3206f6fbd459bd4cd51a
root <root@HP-ENVY-188.localdomain>
2020-12-07 09:56:59 -0500

    #Fixed sort function in Organization column; Added Search filter.

--- app/javascript/components/dashboard/members_view.vue
+++ app/javascript/components/dashboard/members_view.vue
@@ -1,189 +1,172 @@
 <template>
-  <div id="members">
-    <div class="container mt-4">
-      <h3 class="mt-3 mb-1"><span><i class="fas fa-users mr-2"></i></span>Team</h3>
-      <button
-        @click.prevent="download"
-        id="printBtn"
-        class="btn btn-sm btn-dark mb-3">
-        <font-awesome-icon icon="file-pdf" class="mr-2" />
-        Export to PDF
-      </button>
-      <div class="float-right">
-        <button class="btn btn-md btn-info mb-3 team-total">
-        Team Total: {{teamMembers.length}}
-        </button>
-      </div>
-      <div>
-        <table ref="table" id="teamMembersList" class="table-bordered">
-          <thead>
-            <tr class="teamHead">
-              <th class="sort-th col-1" @click="sort('id')">#<i class="fas fa-sort scroll"></i></th>
-              <th class="sort-th" @click="sort('firstName')">First Name<i class="fas fa-sort scroll"></i></th>
-              <th class="sort-th" @click="sort('lastName')">Last Name<i class="fas fa-sort scroll"></i> </th>
-              <th class="sort-th" @click="sort('title')">Position<i class="fas fa-sort scroll ml-2"></i></th>
-              <th class="sort-th" @click="sort('organization')">Organization<i class="fas fa-sort scroll"></i></th>
-              <th class="sort-th" @click="sort('email')">Email<i class="fas fa-sort scroll" ></i></th>
-              <th class="sort-th" @click="sort('phoneNumber')">Phone Number<i class="fas fa-sort scroll"></i></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="value in sortedTeamMembers" :key="value.id" class="teamTable">
-              <td id="col-1-row">{{ value.id }}</td>
-              <td>{{ value.firstName }}</td>
-              <td>{{ value.lastName }}</td>
-              <td>{{ value.title }}</td>
-              <td>{{ value.organization }}</td>
-              <td><a :href="`mailto:${value.email}`">{{ value.email }}</a></td>
-              <td><a :href="`tel:${value.phoneNumber}`">{{ value.phoneNumber }}</a></td>
-            </tr>
-          </tbody>
-        </table>
-        <div class="float-right my-3">
-          <button class="btn btn-sm page-btns h-auto" @click="prevPage"><i class="fas fa-angle-left"></i></button>
-          <button class="btn btn-sm page-btns h-auto disabled" id="page-count">Page {{ currentPage }} of {{ Math.ceil(this.teamMembers.length / pageSize) }} </button>
-          <button class="btn btn-sm page-btns h-auto" @click="nextPage"><i class="fas fa-angle-right"></i></button>
-        </div>
-      </div>
-    </div>
+  <div>
+     <div class="container mt-2">          
+          <h3 class="mt-1 mb-1"><span><i class="fas fa-users mr-2"></i></span>Team</h3>
+          <button
+            @click.prevent="download"
+            id="printBtn"
+            class="btn btn-sm btn-dark mb-1">
+            <font-awesome-icon icon="file-pdf" class="mr-2" />
+            Export to PDF
+          </button>  
+           
+          <b-row class="search-row pt-2 pb-0 pl-3">  
+          <b-col sm="6" md="4" lg="4" class="my-1 pl-0">           
+            <b-input-group size="sm" style="height:38px">
+               <div class="input-group-prepend">
+                <span class="input-group-text" id="search-addon"><i class="fa fa-search"></i></span>
+               </div>
+              <b-form-input
+                v-model="filter"
+                style="height:38px"
+                type="search"
+                id="filterInput"
+                placeholder="Search Team Members"
+              ></b-form-input>                  
+            </b-input-group>    
+          </b-col>
+
+        <b-col class="my-1 pl-3 float-right justify-content-end">           
+        <div class="float-right">
+            <button class="btn btn-md btn-info mb-3 team-total">
+            Team Total: {{items.length}}
+            </button>
+          </div>  
+          <b-col sm="5" md="6" lg="6" class="my-1">
+        <b-form-group
+          label="Team Members per page"
+          label-cols-sm="6"
+          label-cols-md="6"
+          label-cols-lg="6"
+          label-align-sm="right"
+          label-size="sm"
+          label-for="perPageSelect"
+          class="mb-0"
+        >
+          <b-form-select
+            v-model="perPage"
+            id="perPageSelect"
+            size="sm"
+            :options="pageOptions"
+          ></b-form-select>
+        </b-form-group>
+      </b-col> 
+        </b-col>
+        </b-row>        
+        <!-- <paginate ref="paginator" name="items" :list="items" :per="perPage" class="paginate-list" :key="items ? items.length : 1"> -->
+        <b-table sticky-header hover 
+          class="btable"   
+          show-empty   
+          sort-icon-left    
+          :items="items"
+          :fields="fields"
+          :sort-by.sync="sortBy"
+          :sort-desc.sync="sortDesc"
+          :current-page="currentPage"
+          :per-page="perPage"
+          sort-icon-right
+          :filter="filter"
+          :filter-included-fields="filterOn"         
+          :sort-direction="sortDirection"
+          @filtered="onFiltered"
+          responsive="md"   
+        >              
+        </b-table>  
+                 
+          <div>
+          <small>Sorting By: <b>{{ sortBy }}</b> | Sort Direction:
+          <b>{{ sortDesc ? 'Descending' : 'Ascending' }}</b></small>
+           <b-pagination
+          v-model="currentPage"
+          :total-rows="totalRows"
+          :per-page="perPage"         
+          align="fill"
+          size="sm"
+          class="my-0 float-right"         
+        ></b-pagination>
+        </div>     
+    
+        
+   </div>
   </div>
 </template>
 
 <script>
-  import {mapGetters, mapActions} from 'vuex'
-  import {jsPDF} from "jspdf"
-  import 'jspdf-autotable'
-  import {library} from '@fortawesome/fontawesome-svg-core'
-  import {faFilePdf} from '@fortawesome/free-solid-svg-icons'
-  library.add(faFilePdf)
+import {mapGetters, mapActions} from 'vuex'
+import {jsPDF} from "jspdf"
+import 'jspdf-autotable'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faFilePdf} from '@fortawesome/free-solid-svg-icons'
+library.add(faFilePdf)
   export default {
     name: "TeamMembersView",
     data() {
       return {
-        pageSize: 25,
+        sortBy: 'id',
+        sortDesc: false,
+        sortDirection: 'asc',
+        pageOptions: [5, 15, 25, 50,  { value: 100, text: "+100" }],
+        totalRows: 1,        
         currentPage: 1,
-        currentSort: 'id',
-        currentSortDir: 'asc',
+        filter: null,
+        filterOn: [],
+        perPage: 15,
+        fields: [
+          { key: 'id', label:"#", sortable: true, class: 'text-left' },
+          { key: 'firstName', sortable: true, class: 'text-left' },
+          { key: 'lastName', sortable: true, class: 'text-left' },
+          { key: 'title', sortable: true, label: 'Position', class: 'text-left' },
+          { key: 'organization', sortable: true,class: 'text-left' },
+          { key: 'email', sortable: true, class: 'text-left' },
+          { key: 'phoneNumber', sortable: true, class: 'text-left' }
+        ], 
       }
-    },
-    methods: {
-      sort(s) {
-        if (s === this.currentSort) {
-          this.currentSortDir = this.currentSortDir === 'asc' ? 'desc' : 'asc';
-        }
-        this.currentSort = s;
-      },
-      nextPage() {
-        if ((this.currentPage*this.pageSize) < this.teamMembers.length) this.currentPage++;
-      },
-      prevPage() {
-        if(this.currentPage > 1) this.currentPage--;
-      },
-      download() {
-        const doc = new jsPDF("l")
-        const html = this.$refs.table.innerHTML
-        doc.autoTable({ html: '#teamMembersList' })
-        doc.save("Team_Members_list.pdf")
-      }
-    },
+    }, 
+     mounted() {    
+      this.totalRows = this.items.length
+    },  
     computed: {
       ...mapGetters([
         'projectUsers'
       ]),
-      teamMembers() {
+      items() {
         return this.projectUsers
       },
-      sortedTeamMembers() {
-        return this.teamMembers.sort((a,b) => {
-          let modifier = 1;
-          if (this.currentSortDir === 'desc') modifier = -1;
-          if (a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
-          if (a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
-          return 0;
-        }).filter((row, index) => {
-          let start = (this.currentPage-1) * this.pageSize;
-          let end = this.currentPage * this.pageSize;
-          if (index >= start && index < end) return true;
-          return end
-        });
+       sortOptions() {
+        // Create an options list from our fields
+        return this.fields
+          .filter(f => f.sortable)
+          .map(f => {
+            return { text: f.label, value: f.key }
+          })
       }
+    },  
+     methods: {
+        onFiltered(filteredItems) {
+        // Trigger pagination to update the number of buttons/pages due to filtering
+        this.totalRows = filteredItems.length
+        this.currentPage = 1
+      },      
     }
   }
 </script>
-
 <style scoped lang="scss">
- table {
+ /deep/.btable {
     table-layout: fixed;
+    padding-top: 0px;
+    margin-top:0;
     width: 100%;
+    // min-height: 450px !important;
     box-shadow: 0 2.5px 5px rgba(56,56, 56,0.19), 0 3px 3px rgba(56,56,56,0.23);
+    overflow-y:auto !important;
+  } 
+  /deep/thead {
+    background-color:#ededed !important;
+    height: 60px !important;
   }
-  .col-1 {
-    width: 6%
-  }
-  #col-1-row {
-    text-align: center;
-  }
-  thead {
-    background-color: #ededed;
-  }
-  .sort-th {
-    font-size: 1rem !important;
-    text-align: center;
-  }
-  .teamHead {
-    height: 40px;
-  }
-  td {
-    overflow-wrap: break-word;
+  /deep/td {
+    overflow-wrap: break-word !important;
     padding: 5px !important;
-  }
- .scroll {
-    cursor:pointer !important;
-    top: 12px;
-    right: 5px;
-    position:absolute;
-    font-size: 1rem;
-    color: #383838 !important;
-    padding-left:4px !important
-  }
-  /deep/.sort-th {
-    position:relative;
-    padding: 3px;
-    cursor: pointer;
-    letter-spacing: 2px;
-  }
-  .teamTable:hover {
-    cursor: default;
-    background-color: rgba(91, 192, 222, 0.3);
-    border-left: solid rgb(91, 192, 222);
-  }
-  a:visited, a:active, a:hover {
-    text-decoration-line: none !important;
-  }
-  th > a {
-    color: #383838 !important;
-  }
-  .page-btns {
-    width: 20px;
-    line-height: 1 !important;
-    border: none !important;
-    height: 25px;
-    margin-right: 1px;
-    background-color: white;
-    box-shadow: 0 5px 10px rgba(56,56, 56,0.19), 0 6px 6px rgba(56,56,56,0.23);
-    color: #383838;
-    cursor: pointer;
-  }
-  .page-btns:hover {
-    background-color: #ededed;
-  }
-  #page-count {
-    width: auto !important;
-    cursor: default;
-  }
-  .page-btns.active  {
-    background-color: rgba(211, 211, 211, 10%);
-    border: none !important;
   }
   .team-total {
     cursor: default;
