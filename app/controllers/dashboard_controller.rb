5e395936f356fa1ca165fa55979aa0c1945be123
Amand Singh <amand.singh@microhealthllc.com>
2020-12-15 17:05:33 -0500

    fix issue with admin panel

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,17 @@
 class DashboardController < AuthenticatedController
   def index
+    # project_ids = ProjectUser.where(user_id: current_user.id).map(&:project_id).compact.uniq
+    # @active_projects = Project.includes(:facilities, facility_projects: [:tasks, :status]).where(id: project_ids).active
+
     project_ids = ProjectUser.where(user_id: current_user.id).map(&:project_id).compact.uniq
-    @active_projects = Project.includes(:facilities, facility_projects: [:tasks, :status]).where(id: project_ids).active
+    @active_projects = Project.includes(:facilities, facility_projects: [:tasks, :status, :facility]).where(id: project_ids).active
+    @facility_projects_hash = FacilityProject.where(project_id: @active_projects.map(&:id)).group_by(&:project_id)
+    @facilities = Facility.where(id: @facility_projects_hash.values.flatten.map(&:facility_id)).active
+    @facility_projects_hash.each do |project_id, facility_projects|
+      fids = facility_projects.map(&:facility_id).compact.uniq
+      f = @facilities.select{|ff| fids.include?(ff.id) }
+      @facility_projects_hash[project_id] = f  
+    end
+
   end
 end
