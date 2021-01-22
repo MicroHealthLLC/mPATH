bc1b7588f63712536cb4636ddf1081844be4a842
Christopher Calderon <christopher.calderon@microhealthllc.com>
2021-01-22 10:59:30 -0800

    Reapplied map zoom changes.

--- app/javascript/components/dashboard/map_view.vue
+++ app/javascript/components/dashboard/map_view.vue
@@ -26,46 +26,66 @@
               mapTypeControl: false,
               scaleControl: true,
               streetViewControl: false,
-              fullscreenControl: true
+              fullscreenControl: true,
+              restriction: {
+                latLngBounds: {
+                  north: 85,
+                  south: -85,
+                  west: -179.5,
+                  east: 179.5,
+                },
+                strictBounds: true,
+              },
             }"
             @click="resetView"
-            >
+            @bounds_changed="captureMapBounds($event)"
+          >
             <GmapCluster
               :averageCenter="true"
               :enableRetinaIcons="true"
               :ignoreHidden="true"
               :zoomOnClick="true"
-              >
+            >
               <GmapMarker
+                ref="marker"
                 :key="`${facility.id}__${index}`"
                 :animation="4"
-                v-for="(facility, index) in filterFacilitiesWithActiveFacilityGroups"
+                v-for="(
+                  facility, index
+                ) in filterFacilitiesWithActiveFacilityGroups"
                 :position="getLatLngForFacility(facility)"
                 @click="showFacility(facility)"
                 @mouseover="toggleTooltip(facility, `${facility.id}__${index}`)"
-                @mouseout="tooltip.opened=false"
-                :icon="{url: getStatusIconLink(facility)}"
+                @mouseout="tooltip.opened = false"
+                :icon="{ url: getStatusIconLink(facility) }"
               />
             </GmapCluster>
             <GmapInfoWindow
               :options="tooltip.options"
               :position="tooltip.position"
               :opened="tooltip.opened"
-              @closeclick="tooltip.opened=false"
-              >
-              {{tooltip.content}}
+              @closeclick="tooltip.opened = false"
+            >
+              {{ tooltip.content }}
             </GmapInfoWindow>
           </GmapMap>
         </div>
         <div id="rollup-sidebar" class="col-4 p-0" :style="rollupStyle">
-          <div style="margin-left: 20px;">
+          <div style="margin-left: 20px">
             <div>
-              <facility-rollup v-show="!openSidebar"></facility-rollup>
+              <FacilityRollup v-show="!openSidebar"></FacilityRollup>
 
-                <div class="knocker_side" :style="knockerStyle">
-                <button v-if="currentFacility && currentFacility.id" class="knocker btn btn-sm text-light p-1" @click="toggleOpenSideBar">
-                  <small><span class="pr-1"><i class="fas fa-building"></i></span>FACILITY  SUMMARY</small>
-                </button>             
+              <div class="knocker_side" :style="knockerStyle">
+                <button
+                  v-if="currentFacility && currentFacility.id"
+                  class="knocker btn btn-sm text-light p-1"
+                  @click="toggleOpenSideBar"
+                >
+                  <small
+                    ><span class="pr-1"><i class="fas fa-building"></i></span
+                    >FACILITY SUMMARY</small
+                  >
+                </button>
                 <div id="map-sidebar" class="shadow-sm mr-2">
                   <facility-show
                     v-if="currentFacility && currentFacility.id"
@@ -86,17 +106,36 @@
           ref="facilitiesAccordion"
           :hide-close-button="true"
           :blocking="true"
-          >
+        >
           <div v-if="currentFacilityGroup && currentFacilityGroup.id">
-            <div class="facility_grp_close_btn" @click.prevent="onCloseAccordion">
+            <div
+              class="facility_grp_close_btn"
+              @click.prevent="onCloseAccordion"
+            >
               <i class="fa fa-times"></i>
             </div>
-            <h3 class="mb-3 text-break">{{currentFacilityGroup.name}} <span class="badge badge-secondary badge-pill">{{facilityGroupFacilities(currentFacilityGroup).length}}</span></h3>
-            <div v-if="facilityGroupFacilities(currentFacilityGroup) && facilityGroupFacilities(currentFacilityGroup).length == 0" class="mt-3 text-danger">
+            <h3 class="mb-3 text-break">
+              {{ currentFacilityGroup.name }}
+              <span class="badge badge-secondary badge-pill">{{
+                facilityGroupFacilities(currentFacilityGroup).length
+              }}</span>
+            </h3>
+            <div
+              v-if="
+                facilityGroupFacilities(currentFacilityGroup) &&
+                facilityGroupFacilities(currentFacilityGroup).length == 0
+              "
+              class="mt-3 text-danger"
+            >
               There is no facility under this group
             </div>
             <div v-else>
-              <div v-for="facility in facilityGroupFacilities(currentFacilityGroup)">
+              <div
+                v-for="(facility, index) in facilityGroupFacilities(
+                  currentFacilityGroup
+                )"
+                :key="index"
+              >
                 <accordion
                   :expanded="expandedFacility.id"
                   :facility="facility.facility"
@@ -115,28 +154,27 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/markerclustererplus/2.1.4/markerclusterer.js"></script>
 <script>
-import FacilityShow from './facilities/facility_show'
-import FacilityGroupBar from './../shared/facility_group_bar'
-import Accordion from './../shared/accordion'
-import FacilityRollup from './facilities/facility_rollup'
-import {SweetModal} from 'sweet-modal-vue'
-import {mapGetters, mapMutations} from 'vuex'
-import * as Moment from 'moment'
-import {extendMoment} from 'moment-range'
-const moment = extendMoment(Moment)
-
+import FacilityShow from "./facilities/facility_show";
+import FacilityGroupBar from "./../shared/facility_group_bar";
+import Accordion from "./../shared/accordion";
+import FacilityRollup from "./facilities/facility_rollup";
+import { SweetModal } from "sweet-modal-vue";
+import { mapGetters, mapMutations } from "vuex";
+import * as Moment from "moment";
+import { extendMoment } from "moment-range";
+const moment = extendMoment(Moment);
 export default {
-  name: 'ProjectMapView',
+  name: "ProjectMapView",
   components: {
     FacilityShow,
     FacilityGroupBar,
     Accordion,
     FacilityRollup,
-    SweetModal
+    SweetModal,
   },
   data() {
     return {
-      center: {lat: 40.64, lng: -74.66},
+      center: { lat: 40.64, lng: -74.66 },
       zoom: 3,
       openSidebar: false,
       expandedFacility: {},
@@ -148,202 +186,265 @@ export default {
         options: {
           pixelOffset: {
             width: 0,
-            height: -30
-          }
-        }
-      }
-    }
+            height: -30,
+          },
+        },
+      },
+      mapBounds: {
+        south: 0,
+        west: 0,
+        north: 0,
+        east: 0,
+      },
+      initialFacilities: [],
+      facilitiesSet: false,
+      mapFacilityCount: null,
+    };
   },
   computed: {
     ...mapGetters([
-      'contentLoaded',
-      'facilities',
-      'facilityGroups',
-      'statuses',
-      'currentFacility',
-      'currentFacilityGroup',
-      'currentProject',
-      'filteredFacilityGroups',
-      'filterFacilitiesWithActiveFacilityGroups',
-      'filteredFacilities',
-      'facilityGroupFacilities'
+      "contentLoaded",
+      "facilities",
+      "facilityGroups",
+      "statuses",
+      "currentFacility",
+      "currentFacilityGroup",
+      "currentProject",
+      "filteredFacilityGroups",
+      "filterFacilitiesWithActiveFacilityGroups",
+      "filteredFacilities",
+      "facilityGroupFacilities",
+      "getMapZoomFilter",
     ]),
     knockerStyle() {
-      return this.openSidebar ? {} : {transform: "translateX(calc(105% - 20px))"}
+      return this.openSidebar
+        ? {}
+        : { transform: "translateX(calc(105% - 20px))" };
     },
     rollupStyle() {
-      return this.openSidebar ? {right: '12px'} : {right: '0'}
+      return this.openSidebar ? { right: "12px" } : { right: "0" };
     },
   },
   methods: {
     ...mapMutations([
-      'updateFacilities',
-      'setCurrentFacilityGroup',
-      'setCurrentFacility'
+      "updateFacilities",
+      "setCurrentFacilityGroup",
+      "setCurrentFacility",
+      "setMapZoomFilter",
+      "setFacilities",
     ]),
     getLatLngForFacility(facility) {
-      return {lat: Number(facility.lat), lng: Number(facility.lng)}
+      return { lat: Number(facility.lat), lng: Number(facility.lng) };
     },
     showFacility(facility) {
-      this.openSidebar = true
-      if (this.currentFacility && facility.id == this.currentFacility.id) return;
-      this.setCurrentFacilityGroup(this.facilityGroups.find(fg => fg.id == facility.facilityGroupId))
-      this.center = this.getLatLngForFacility(facility)
-      this.$refs.googlemap.panTo(this.center)
-      this.setCurrentFacility(facility)
+      this.openSidebar = true;
+      if (this.currentFacility && facility.id == this.currentFacility.id)
+        return;
+      this.setCurrentFacilityGroup(
+        this.facilityGroups.find((fg) => fg.id == facility.facilityGroupId)
+      );
+      this.center = this.getLatLngForFacility(facility);
+      this.$refs.googlemap.panTo(this.center);
+      this.setCurrentFacility(facility);
     },
     closeSidebar() {
-      this.openSidebar = false
-      this.center = this.getLatLngForFacility(this.currentFacility)
-      this.setCurrentFacility(null)
+      this.openSidebar = false;
+      this.center = this.getLatLngForFacility(this.currentFacility);
+      this.setCurrentFacility(null);
     },
     gotoFacilityGroup(facilityGroup) {
-      this.setCurrentFacilityGroup(facilityGroup)
-      this.$refs.facilitiesAccordion.open()
+      this.setCurrentFacilityGroup(facilityGroup);
+      this.$refs.facilitiesAccordion.open();
     },
     updateFacility(facility) {
-      this.setCurrentFacility(facility)
-      let index = this.facilities.findIndex(f => f.id == facility.id)
-      if (index > -1) this.updateFacilities({index, facility})
+      this.setCurrentFacility(facility);
+      let index = this.facilities.findIndex((f) => f.id == facility.id);
+      if (index > -1) this.updateFacilities({ index, facility });
     },
     updateExpanded(facility) {
-      if (facility.id === this.expandedFacility.id) this.expandedFacility = {}
-      else this.expandedFacility = facility
+      if (facility.id === this.expandedFacility.id) this.expandedFacility = {};
+      else this.expandedFacility = facility;
     },
     toggleOpenSideBar() {
-      this.openSidebar = this.currentFacility ? !this.openSidebar : false
+      this.openSidebar = this.currentFacility ? !this.openSidebar : false;
     },
     getStatusIconLink(facility) {
-      return 'https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=|'+ facility.color.split('#')[1]
+      return (
+        "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=|" +
+        facility.color.split("#")[1]
+      );
     },
     resetView() {
       if (this.openSidebar) {
-        this.setCurrentFacility(null)
-        this.openSidebar = false
+        this.setCurrentFacility(null);
+        this.openSidebar = false;
       }
     },
     toggleTooltip(marker, key) {
-      this.tooltip.position = this.getLatLngForFacility(marker)
-      this.tooltip.content = marker.facilityName
-      this.tooltip.opened = true
-      this.tooltip.key = key
+      this.tooltip.position = this.getLatLngForFacility(marker);
+      this.tooltip.content = marker.facilityName;
+      this.tooltip.opened = true;
+      this.tooltip.key = key;
     },
     onCloseAccordion() {
-      this.$refs.facilitiesAccordion && this.$refs.facilitiesAccordion.close()
-    }
-  }
+      this.$refs.facilitiesAccordion && this.$refs.facilitiesAccordion.close();
+    },
+    captureMapBounds(payload) {
+      // Record initial facilities state and set flag
+      if (!this.facilitiesSet && this.contentLoaded) {
+        this.initialFacilities = this.facilities;
+        this.facilitiesSet = true;
+        this.mapFacilityCount = this.initialFacilities.length;
+      }
+      // Verify bounds_changed event payload is not undefined
+      if (payload && this.facilitiesSet) {
+        var bounds = JSON.parse(JSON.stringify(payload));
+        this.mapBounds.south = bounds.south;
+        this.mapBounds.west = bounds.west;
+        this.mapBounds.north = bounds.north;
+        this.mapBounds.east = bounds.east;
+        // Update array of Ids of facilities that are visible on map
+        this.setMapZoomFilter(this.visibleMarkers());
+        // Set facilities at state level by filtering out non-visible facilities
+        if (this.getMapZoomFilter.length !== this.mapFacilityCount) {
+          this.setFacilities(
+            this.initialFacilities.filter((item) =>
+              this.getMapZoomFilter.includes(item.id)
+            )
+          );
+          this.mapFacilityCount = this.facilities.length;
+        }
+      }
+    },
+    visibleMarkers() {
+      return this.initialFacilities
+        .filter(
+          (item) =>
+            Number(item.lng) > this.mapBounds.west &&
+            Number(item.lng) < this.mapBounds.east &&
+            Number(item.lat) < this.mapBounds.north &&
+            Number(item.lat) > this.mapBounds.south
+        )
+        .map((item) => Number(item.id));
+    },
+  },
+  beforeDestroy() {
+    this.setFacilities(this.initialFacilities);
+  },
 };
 </script>
 
 <style scoped lang="scss">
-  #_wrapper {
-    padding-top: 1px;
+#_wrapper {
+  padding-top: 1px;
+  height: calc(100vh - 94px);
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+}
+#map-wrap {
+  height: calc(100vh - 94px);
+  width: 69vw;
+}
+#rollup-sidebar {
+  > div {
     height: calc(100vh - 94px);
-    width: 100%;
-    display: flex;
-    flex-wrap: wrap;
-  }
-  #map-wrap {
-    height: calc(100vh - 94px);
-    width: 69vw;
-  }
-  #rollup-sidebar {
-    > div {
-      height: calc(100vh - 94px);
-      z-index: 1000;
-    }
-    position: absolute;
-    top: 0;
-    overflow-x: hidden;
-    overflow-y: auto;
-  }
-  #map-sidebar {
     z-index: 1000;
-    height: calc(100vh - 94px);
-    overflow: auto;
-    background: white;
-    padding: 10px;
   }
-  .knocker_side {
+  position: absolute;
+  top: 0;
+  overflow-x: hidden;
+  overflow-y: auto;
+}
+#map-sidebar {
+  z-index: 1000;
+  height: calc(100vh - 94px);
+  overflow: auto;
+  background: white;
+  padding: 10px;
+}
+.knocker_side {
+  position: absolute;
+  width: 95%;
+  top: 0;
+  transition: 0.3s ease;
+  height: 100%;
+}
+.knocker {
+  cursor: pointer;
+  z-index: 1000 !important;
+  position: absolute;
+  top: 70%;
+  left: -63.8px;
+  width: fit-content;
+  display: flex;
+  border-bottom-right-radius: 0 !important;
+  border-bottom-left-radius: 0 !important;
+  -moz-transform: rotate(-90deg);
+  -ms-transform: rotate(-90deg);
+  -o-transform: rotate(-90deg);
+  -webkit-transform: rotate(-90deg);
+  background-color: rgba(65, 184, 131, 0.85);
+}
+/* sidebar transitions */
+.slide-fade-enter-active {
+  transition: all 0.2s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-enter,
+.slide-fade-leave-to {
+  transform: translateX(100px);
+  opacity: 0;
+}
+.roll-fade-enter-active {
+  transition: all 0.2s ease;
+}
+.roll-fade-leave-active {
+  transition: all 0.3s ease;
+}
+.roll-fade-enter,
+.roll-fade-leave-to {
+  opacity: 0;
+  transform: translate(100px, 50px);
+}
+.regions-bar {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  z-index: 800;
+}
+.facility_accordion_modal.sweet-modal-overlay /deep/ .sweet-modal {
+  min-width: 50vw;
+  .sweet-content {
+    padding-top: 30px;
+  }
+  .facility_grp_close_btn {
+    display: flex;
     position: absolute;
-    width: 95%;
-    top: 0;
-    transition: .3s ease;
-    height: 100%;
+    top: 15px;
+    left: 15px;
+    cursor: pointer;
   }
-  .knocker {
-   cursor: pointer;
-   z-index: 1000 !important;
-    position: absolute;
-    top: 70%;  
-    left: -63.8px;
-    width: fit-content;
-    display: flex;   
-    border-bottom-right-radius: 0 !important;
-    border-bottom-left-radius: 0 !important;    
-    -moz-transform:rotate(-90deg);
-    -ms-transform:rotate(-90deg);
-    -o-transform:rotate(-90deg);
-    -webkit-transform:rotate(-90deg);
-    background-color: rgba(65, 184, 131, .85);
- }
-  /* sidebar transitions */
-  .slide-fade-enter-active {
-    transition: all .2s ease;
-  }
-  .slide-fade-leave-active {
-    transition: all .3s ease;
-  }
-  .slide-fade-enter, .slide-fade-leave-to {
-    transform: translateX(100px);
-    opacity: 0;
-  }
-  .roll-fade-enter-active {
-    transition: all .2s ease;
-  }
-  .roll-fade-leave-active {
-    transition: all .3s ease;
-  }
-  .roll-fade-enter, .roll-fade-leave-to {
-    opacity: 0;
-    transform: translate(100px, 50px);
-  }
-  .regions-bar {
-    position: absolute;
-    top: 10px;
-    left: 10px;
-    z-index: 800;
-  }
-  .facility_accordion_modal.sweet-modal-overlay /deep/ .sweet-modal {
-    min-width: 50vw;
-    .sweet-content {
-      padding-top: 30px;
-    }
-    .facility_grp_close_btn {
-      display: flex;
-      position: absolute;
-      top: 15px;
-      left: 15px;
-      cursor: pointer;
-    }
-  }
-  // .knocker {
-  //   z-index: 9999999;
-  //   cursor: pointer;
-  //   position: absolute;
-  //   top: 40%;
-  //   left: -12px;
-  //   width: 12px;
-  //   border-top-left-radius: 12px;
-  //   border-bottom-left-radius: 12px;
-  //   height: 100px;
-  //   background: #ffa500;
-  //   display: flex;
-  //   align-items: center;
-  //   justify-content: center;  
-  // }
-  .vue-map-container /deep/ button.gm-ui-hover-effect {
-    display: none;
-    visibility: hidden;
-  }
+}
+// .knocker {
+//   z-index: 9999999;
+//   cursor: pointer;
+//   position: absolute;
+//   top: 40%;
+//   left: -12px;
+//   width: 12px;
+//   border-top-left-radius: 12px;
+//   border-bottom-left-radius: 12px;
+//   height: 100px;
+//   background: #ffa500;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+// }
+.vue-map-container /deep/ button.gm-ui-hover-effect {
+  display: none;
+  visibility: hidden;
+}
 </style>
