5ce64c5ab03b92906b1d8c42694d6f617dff5eac
bilel-kedidi <bilel.kedidi@gmail.com>
2021-01-26 13:01:51 +0500

    Added sorting test cases for risk stages

--- spec/cypress/integration/admin_panel/risks/risk_stages.js
+++ spec/cypress/integration/admin_panel/risks/risk_stages.js
@@ -54,7 +54,7 @@ describe('Admin Panel Risk Stages', function() {
     cy.get('.action_item > a').contains('New Risk Stage').click()
     cy.get('#risk_stage_name').type('New Test Risk Stage').should('have.value', 'New Test Risk Stage')
     cy.get('#risk_stage_submit_action').contains('Create Risk stage').click()
-    cy.get('#index_table_risk_stages > tbody > tr').first().within(() => {
+    cy.get('#index_table_risk_stages > tbody > tr').last().within(() => {
       cy.get('.col-actions').contains('Delete').click()
     })
     cy.get('.flashes').contains('Risk stage was successfully destroyed.').should('be.visible')
@@ -62,15 +62,35 @@ describe('Admin Panel Risk Stages', function() {
     cy.get('#logout').click()
   })
 
-  // it('Search Risk Stage contains name', function() {
-  //   cy.get('#q_name').type('Test Risk Stage').should('have.value', 'Test Risk Stage')
-  //   cy.get('[type=submit]').first().contains('Filter').click()
-  //   cy.get('#search_status_sidebar_section').should('be.visible')
-  //   cy.get('#search_status_sidebar_section > h3').contains('Search status:').should('be.visible')
-  //   cy.get('h4').contains('Current filters:').should('be.visible')
-  //   cy.get('.current_filter').contains('Name contains Test Risk Stage').should('be.visible')
-  //   cy.get('#index_table_risk_stages > tbody > tr').its('length').should('be.eq', 1)
-  //   cy.get('.clear_filters_btn').last().contains('Clear Filters').click()
-  //   cy.get('#logout').click()
-  // })
+  it('Sort Risk Stage according to Name', function() {
+    cy.get('.sortable').contains('Name').click()
+    cy.get('#index_table_risk_stages > tbody > tr').first().contains('Test Risk Stage').should('be.visible')
+    cy.get('.sortable').contains('Name').click()
+    cy.get('#index_table_risk_stages > tbody > tr').first().contains('New Risk Stage').should('be.visible')
+    cy.get('.sortable').contains('Name').click()
+    cy.get('#index_table_risk_stages > tbody > tr').first().contains('Test Risk Stage').should('be.visible')
+    cy.get('#logout').click()
+  })
+
+  it('Sort Risk Stage according to Percentage', function() {
+    cy.get('.sortable').contains('Percentage').click()
+    cy.get('#index_table_risk_stages > tbody > tr').first().contains(40).should('be.visible')
+    cy.get('.sortable').contains('Percentage').click()
+    cy.get('#index_table_risk_stages > tbody > tr').first().contains(60).should('be.visible')
+    cy.get('.sortable').contains('Percentage').click()
+    cy.get('#index_table_risk_stages > tbody > tr').first().contains(40).should('be.visible')
+    cy.get('#logout').click()
+  })
+
+  it('Search Risk Stage contains name', function() {
+    cy.get('#q_name').type('Test Risk Stage').should('have.value', 'Test Risk Stage')
+    cy.get('[type=submit]').first().contains('Filter').click()
+    cy.get('#search_status_sidebar_section').should('be.visible')
+    cy.get('#search_status_sidebar_section > h3').contains('Search status:').should('be.visible')
+    cy.get('h4').contains('Current filters:').should('be.visible')
+    cy.get('.current_filter').contains('Name contains Test Risk Stage').should('be.visible')
+    cy.get('#index_table_risk_stages > tbody > tr').its('length').should('be.eq', 1)
+    cy.get('.clear_filters_btn').last().contains('Clear Filters').click()
+    cy.get('#logout').click()
+  })
 })
