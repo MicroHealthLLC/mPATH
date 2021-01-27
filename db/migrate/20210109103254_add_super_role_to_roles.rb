3ab80a5d2be3de618c9289c3626c28b3ab0e7030
bilel-kedidi <bilel.kedidi@gmail.com>
2021-01-27 16:53:33 +0100

    Update 20210109103254_add_super_role_to_roles.rb

--- db/migrate/20210109103254_add_super_role_to_roles.rb
+++ db/migrate/20210109103254_add_super_role_to_roles.rb
@@ -1,7 +1,9 @@
 class AddSuperRoleToRoles < ActiveRecord::Migration[5.2]
   def change
+     begin
     add_column :roles, :system, :boolean, default: false
-
+ rescue
+    end
     begin
       Role.create(name: 'superadmin_role', system: true)
     rescue
