f12b62d0fdfa23b401b1198a03d661f0fd98bf09
bilel-kedidi <bilel.kedidi@gmail.com>
2021-01-27 16:29:43 +0100

    Update 20210106130801_create_project_roles.rb

--- db/migrate/20210106130801_create_project_roles.rb
+++ db/migrate/20210106130801_create_project_roles.rb
@@ -1,6 +1,6 @@
 class CreateProjectRoles < ActiveRecord::Migration[5.2]
   def change
-    create_table :project_roles do |t|
+    create_table :project_roles, force: true do |t|
       t.integer :role_id, foreign_key: true
       t.integer :project_id, foreign_key: true
       t.timestamps
