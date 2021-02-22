29558c476d4689054e0ccd2c397155fa7e69975c
Amand Singh <amand.singh@microhealthllc.com>
2021-02-22 16:41:55 -0500

    adding default status for project

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,8 +1,15 @@
 class Status < SortableRecord
-  default_scope {order(Status.order_humanize)}
+  # default_scope {order(Status.order_humanize)}
   has_many :project_statuses
   has_many :projects, through: :project_statuses
 
   validates :name, presence: true
   validates_uniqueness_of :name, case_sensitive: false
+
+  def self.not_started
+    Status.find_or_create_by(name: "Not Started" ) do |s|
+      s.color = "#000000"
+    end
+  end
+
 end
