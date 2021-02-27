af099b86b7c1bfb4e6a11eddecb7688000007a43
Christopher Calderon <christopher.calderon@microhealthllc.com>
2021-02-26 16:08:58 -0800

    Packaged context-menu functions and data into ContextMenu component.

--- app/javascript/components/dashboard/tasks/task_sheets.vue
+++ app/javascript/components/dashboard/tasks/task_sheets.vue
@@ -27,58 +27,12 @@
         </td>       
         <td v-else class="twenty">No Updates</td>
       </tr>
+
       <!-- The context-menu appears only if table row is right-clicked -->
-      <context-menu :display="showContextMenu" ref="menu">
-        <el-menu collapse>
-          <el-menu-item @click="editTask">Open</el-menu-item>
-          <hr>
-          <el-menu-item @click="createDuplicate" :disabled="!$permissions.tasks.write">Duplicate</el-menu-item>
-          <el-submenu index="1" :disabled="!$permissions.tasks.write">
-            <template slot="title">
-              <span slot="title">Duplicate to...</span>
-            </template>
-            <div>
-              <div class="menu-subwindow-title">Duplicate to...</div>
-              <el-input class="filter-input" placeholder="Filter Projects..." v-model="filterTree"></el-input>
-              <el-tree
-                :data="treeFormattedData"
-                :props="defaultProps"
-                :filter-node-method="filterNode"
-                @check-change="toggleSubmitBtn"
-                show-checkbox
-                ref="duplicatetree"
-                node-key="id"
-              >
-              </el-tree>
-              <div class="context-menu-btns">
-                <button class="btn btn-sm btn-success ml-2" @click="duplicateSelectedTasks" :disabled="submitDisabled">Submit</button>
-                <button class="btn btn-sm btn-primary ml-2" @click="selectAllNodes">Select All</button>
-                <button class="btn btn-sm btn-outline-secondary ml-2" @click="clearAllNodes">Clear All</button>         
-              </div>
-            </div>
-          </el-submenu>
-          <hr>
-          <el-submenu index="2" :disabled="!$permissions.tasks.write">
-            <template slot="title">
-              <span slot="title">Move to...</span>
-            </template>
-            <div>
-              <div class="menu-subwindow-title">Move to...</div>
-              <el-input class="filter-input" placeholder="Filter Projects..." v-model="filterTree"></el-input>
-              <el-tree
-                :data="treeFormattedData"
-                :props="defaultProps"
-                :filter-node-method="filterNode"
-                ref="movetree"
-                @node-click="move"
-              >
-              </el-tree>
-            </div>
-          </el-submenu>
-          <hr>
-          <el-menu-item @click="deleteTask" :disabled="!$permissions.tasks.delete">Delete</el-menu-item>
-        </el-menu>
-      </context-menu>
+      <ContextMenu :facilities="facilities" :facilityGroups="facilityGroups" :task="task" :display="showContextMenu" ref="menu" @open-task="editTask">
+        
+      </ContextMenu>
+
     </table>
       <div v-if="has_task" class="w-100 action-form-overlay updateForm">
         <task-form
@@ -132,14 +86,7 @@ export default {
       DV_edit_task: {},
       DV_edit_issue: {},
       has_task: false,
-      showContextMenu: false,
-      defaultProps: {
-        children: "children",
-        label: "label",
-        disabled: "disabled"
-      },
-      filterTree: '',
-      submitted: false
+      showContextMenu: false
     };
   },
   mounted() {
@@ -151,15 +98,6 @@ export default {
   methods: {
     ...mapMutations(["updateTasksHash", "setTaskForManager", "setToggleRACI"]),
     ...mapActions(["taskDeleted", "taskUpdated", "updateWatchedTasks"]),
-    deleteTask() {
-      var confirm = window.confirm(
-        `Are you sure, you want to delete "${this.DV_task.text}"?`
-      );
-      if (!confirm) {
-        return;
-      }
-      this.taskDeleted(this.DV_task);
-    },
     openSubTask(subTask) {
       let task = this.currentTasks.find((t) => t.id == subTask.id);
       if (!task) return;
@@ -213,197 +151,6 @@ export default {
       e.preventDefault();
       this.$refs.menu.open(e);
     },
-    moveTask(task, facilityProjectId) {
-      if (!this._isallowed("write")) return;
-      this.$validator.validate().then((success) => {
-        if (!success || this.loading) {
-          this.showErrors = !success;
-          return;
-        }
-
-        this.loading = true;
-        let formData = new FormData();
-
-        formData.append("task[facility_project_id]", facilityProjectId);
-
-        let url = `/projects/${this.currentProject.id}/facilities/${task.facilityId}/tasks/${task.id}.json`;
-        let method = "PUT";
-        let callback = "task-updated";
-
-        axios({
-          method: method,
-          url: url,
-          data: formData,
-          headers: {
-            "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]')
-              .attributes["content"].value,
-          },
-        })
-          .then((response) => {
-            this.$emit(callback, humps.camelizeKeys(response.data.task));
-            this.updateFacilities(
-              humps.camelizeKeys(response.data.task),
-              facilityProjectId
-            );
-            if (response.status === 200) {
-              this.$message({
-                message: `${task.text} was moved successfully.`,
-                type: 'success',
-                showClose: true
-              });
-            }
-          })
-          .catch((err) => {
-            this.$message({
-              message: `Unable to move ${task.text}. Please try again.`,
-              type: 'error',
-              showClose: true
-            });
-            // var errors = err.response.data.errors
-            console.log(err);
-          })
-          .finally(() => {
-            this.loading = false;
-            this.updateTasksHash({ task: task, action: "delete" });
-          });
-      });
-    },
-    updateFacilities(updatedTask, id) {
-      var facilities = this.getUnfilteredFacilities;
-
-      facilities.forEach((facility) => {
-        if (facility.facilityProjectId === id) {
-          facility.tasks.push(updatedTask);
-        }
-      });
-    },
-    updateFacilityTask(task) {
-      var facilities = this.getUnfilteredFacilities;
-
-      var facilityIndex = facilities.findIndex(item => item.facilityProjectId === task.facilityProjectId);
-
-      facilities[facilityIndex].tasks.push(task);
-    },
-    createDuplicate() {
-      let url = `/projects/${this.currentProject.id}/facilities/${this.DV_task.facilityId}/tasks/${this.DV_task.id}/create_duplicate.json`
-      let method = "POST"
-      let callback = "task-created"
-
-      let formData = new FormData()
-      formData.append('id', this.DV_task.id)
-
-      axios({
-        method: method,
-        url: url,
-        data: formData,
-        headers: {
-          'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').attributes['content'].value
-        }
-      })
-      .then((response) => {
-        this.$emit(callback, humps.camelizeKeys(response.data.task))
-        this.updateFacilityTask(
-            humps.camelizeKeys(response.data.task),
-            this.DV_task.facilityProjectId
-          );
-        if (response.status === 200) {
-          this.$message({
-            message: `${this.DV_task.text} was duplicated successfully.`,
-            type: 'success',
-            showClose: true
-          });
-        }
-      })
-      .catch((err) => {
-        this.$message({
-          message: `Unable to duplicate ${this.DV_task.text}. Please try again.`,
-          type: 'error',
-          showClose: true
-        });
-        // var errors = err.response.data.errors
-        console.log(err)
-      })
-      .finally(() => {
-        // this.loading = false
-      })
-    },
-    selectAllNodes() {
-      this.$refs.duplicatetree.setCheckedNodes(this.treeFormattedData)
-    },
-    clearAllNodes() {
-      this.$refs.duplicatetree.setCheckedNodes([])
-    },
-    move(node) {
-      if (!node.hasOwnProperty('children')) {
-        this.moveTask(this.task, node.id)
-      }    
-    },
-    duplicateSelectedTasks() {
-      this.submitted = true;
-
-      var facilityNodes = this.$refs.duplicatetree.getCheckedNodes().filter(item => !item.hasOwnProperty('children'));
-      
-      var ids = facilityNodes.map(facility => facility.id)
-
-      let url = `/projects/${this.currentProject.id}/facilities/${this.DV_task.facilityId}/tasks/${this.DV_task.id}/create_bulk_duplicate?`
-      let method = "POST"
-      let callback = "task-created"
-
-      ids.forEach((id, index) => {
-        if (index === 0) {
-          url += `facility_project_ids[]=${id}`
-        } else {
-          url += `&facility_project_ids[]=${id}`
-        }  
-      })
-
-      let formData = new FormData()
-      formData.append('id', this.DV_task.id)
-      formData.append('facility_project_ids', ids)
-
-      axios({
-        method: method,
-        url: url,
-        data: formData,
-        headers: {
-          'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').attributes['content'].value
-        }
-      })
-      .then((response) => {
-        this.$emit(callback, humps.camelizeKeys(response.data.task))
-
-        response.data.tasks.forEach(task => {
-          this.updateFacilityTask(humps.camelizeKeys(task), task.facilityProjectId)
-        })
-        if (response.status === 200) {
-          this.$message({
-            message: `${this.DV_task.text} was duplicated successfully to selected projects.`,
-            type: 'success',
-            showClose: true
-          });
-        }
-        
-      })
-      .catch((err) => {
-        this.$message({
-          message: `Unable to duplicate ${this.DV_task.text} to selected projects. Please try again.`,
-          type: 'error',
-          showClose: true
-        });
-        // var errors = err.response.data.errors
-        console.log(err)
-      })
-      .finally(() => {
-        // this.loading = false
-      })
-    },
-    filterNode(value, data) {
-      if (!value) return true;
-      return data.label.toLowerCase().indexOf(value.toLowerCase()) !== -1;
-    },
-    toggleSubmitBtn() {
-      this.submitted = false
-    },
     deleteTask() {
       let confirm = window.confirm(`Are you sure you want to delete "${this.DV_task.text}"?`)
       if (!confirm) {return}
@@ -445,32 +192,7 @@ export default {
       return (
         this.managerView.task && this.managerView.task.id == this.DV_task.id
       );
-    },
-    treeFormattedData() {
-      var data = [];
-
-      this.facilityGroups.forEach((group, index) => {
-        data.push({
-          id: index,
-          label: group.name,
-          children: [...group.facilities.filter(facility => facility.facility.id !== this.DV_task.facilityId).map(facility => {
-            return {
-              id: facility.facilityProjectId,
-              label: facility.facilityName
-            }
-          })],
-        });
-      });
-
-      return [...data]    
-    },
-    submitDisabled() {
-      if (this.$refs.duplicatetree) {
-        return this.$refs.duplicatetree.getCheckedNodes().length === 0 || this.submitted
-      } else {
-        return this.submitted
-      }  
-    },
+    }
   },
   watch: {
     task: {
@@ -615,37 +337,5 @@ td {
       background-color: rgba(91, 192, 222, 0.3);
     }
   }
-}
-hr {
-  margin: 0;
-}
-.el-menu-item {
-  padding: 10px;
-  line-height: unset;
-  height: unset;
-  text-align: center;
-  overflow-x: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  &:hover {
-    background-color: rgba(91, 192, 222, 0.3);
-  }
-}
-.context-menu-btns, .filter-input {
-  padding: 10px;
-}
-.el-menu-item {
-  padding-right: 30px;
-}
-.el-tree {
-  padding: 10px;
-  max-width: 300px;
-  max-height: 300px;
-  overflow-y: auto;
-}
-.menu-subwindow-title {
-  font-size: 14px;
-  text-align: center;
-  margin-top: 10px;
 }
 </style>
--- app/javascript/components/shared/ContextMenu.vue
+++ app/javascript/components/shared/ContextMenu.vue
@@ -7,25 +7,115 @@
     tabindex="0"
     @mouseleave="close"
   >
-    <slot></slot>
+    <el-menu collapse>
+      <el-menu-item @click="openTask">Open</el-menu-item>
+      <hr />
+      <el-menu-item
+        @click="createDuplicate"
+        :disabled="!$permissions.tasks.write"
+        >Duplicate</el-menu-item
+      >
+      <el-submenu index="1" :disabled="!$permissions.tasks.write">
+        <template slot="title">
+          <span slot="title">Duplicate to...</span>
+        </template>
+        <div>
+          <div class="menu-subwindow-title">Duplicate to...</div>
+          <el-input
+            class="filter-input"
+            placeholder="Filter Projects..."
+            v-model="filterTree"
+          ></el-input>
+          <el-tree
+            :data="treeFormattedData"
+            :props="defaultProps"
+            :filter-node-method="filterNode"
+            @check-change="toggleSubmitBtn"
+            show-checkbox
+            ref="duplicatetree"
+            node-key="id"
+          >
+          </el-tree>
+          <div class="context-menu-btns">
+            <button
+              class="btn btn-sm btn-success ml-2"
+              @click="duplicateSelectedTasks"
+              :disabled="submitDisabled"
+            >
+              Submit
+            </button>
+            <button class="btn btn-sm btn-primary ml-2" @click="selectAllNodes">
+              Select All
+            </button>
+            <button
+              class="btn btn-sm btn-outline-secondary ml-2"
+              @click="clearAllNodes"
+            >
+              Clear All
+            </button>
+          </div>
+        </div>
+      </el-submenu>
+      <hr />
+      <el-submenu index="2" :disabled="!$permissions.tasks.write">
+        <template slot="title">
+          <span slot="title">Move to...</span>
+        </template>
+        <div>
+          <div class="menu-subwindow-title">Move to...</div>
+          <el-input
+            class="filter-input"
+            placeholder="Filter Projects..."
+            v-model="filterTree"
+          ></el-input>
+          <el-tree
+            :data="treeFormattedData"
+            :props="defaultProps"
+            :filter-node-method="filterNode"
+            ref="movetree"
+            @node-click="move"
+          >
+          </el-tree>
+        </div>
+      </el-submenu>
+      <hr />
+      <el-menu-item @click="deleteTask" :disabled="!$permissions.tasks.delete"
+        >Delete</el-menu-item
+      >
+    </el-menu>
   </div>
 </template>
 
 <script>
 import Vue from "vue";
+import { mapGetters, mapActions, mapMutations } from "vuex";
+import axios from "axios";
+import humps from "humps";
+
 export default {
   name: "ContextMenu",
   props: {
-    display: Boolean, // prop detect if we should show context menu
+    display: Boolean, // prop detect if we should show context menu,
+    facilities: Array,
+    facilityGroups: Array,
+    task: Object,
   },
   data() {
     return {
       left: 0, // left position
       top: 0, // top position
       show: false, // affect display of context menu
+      defaultProps: {
+        children: "children",
+        label: "label",
+        disabled: "disabled",
+      },
+      filterTree: "",
+      submitted: false,
     };
   },
   computed: {
+    ...mapGetters(["currentProject", "getUnfilteredFacilities"]),
     // get position of context menu
     style() {
       return {
@@ -33,8 +123,49 @@ export default {
         left: this.left + "px",
       };
     },
+    treeFormattedData() {
+      var data = [];
+
+      this.facilityGroups.forEach((group, index) => {
+        data.push({
+          id: index,
+          label: group.name,
+          children: [
+            ...group.facilities
+              .filter(
+                (facility) => facility.facility.id !== this.task.facilityId
+              )
+              .map((facility) => {
+                return {
+                  id: facility.facilityProjectId,
+                  label: facility.facilityName,
+                };
+              }),
+          ],
+        });
+      });
+
+      return [...data];
+    },
+    submitDisabled() {
+      if (this.$refs.duplicatetree) {
+        return (
+          this.$refs.duplicatetree.getCheckedNodes().length === 0 ||
+          this.submitted
+        );
+      } else {
+        return this.submitted;
+      }
+    },
+    isAllowed() {
+      return (salut) =>
+        this.$currentUser.role == "superadmin" ||
+        this.$permissions.tasks[salut];
+    },
   },
   methods: {
+    ...mapActions(["taskDeleted"]),
+    ...mapMutations(["updateTasksHash"]),
     // closes context menu
     close() {
       this.show = false;
@@ -58,11 +189,229 @@ export default {
       Vue.nextTick(() => this.$el.focus());
       this.show = true;
     },
+    openTask() {
+      this.$emit("open-task", this.task);
+      this.close();
+    },
+    moveTask(task, facilityProjectId) {
+      if (!this.isAllowed("write")) return;
+      this.$validator.validate().then((success) => {
+        if (!success || this.loading) {
+          this.showErrors = !success;
+          return;
+        }
+
+        this.loading = true;
+        let formData = new FormData();
+
+        formData.append("task[facility_project_id]", facilityProjectId);
+
+        let url = `/projects/${this.currentProject.id}/facilities/${task.facilityId}/tasks/${task.id}.json`;
+        let method = "PUT";
+        let callback = "task-updated";
+
+        axios({
+          method: method,
+          url: url,
+          data: formData,
+          headers: {
+            "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]')
+              .attributes["content"].value,
+          },
+        })
+          .then((response) => {
+            this.$emit(callback, humps.camelizeKeys(response.data.task));
+            this.updateFacilities(
+              humps.camelizeKeys(response.data.task),
+              facilityProjectId
+            );
+            if (response.status === 200) {
+              this.$message({
+                message: `${task.text} was moved successfully.`,
+                type: "success",
+                showClose: true,
+              });
+            }
+          })
+          .catch((err) => {
+            this.$message({
+              message: `Unable to move ${task.text}. Please try again.`,
+              type: "error",
+              showClose: true,
+            });
+            // var errors = err.response.data.errors
+            console.log(err);
+          })
+          .finally(() => {
+            this.loading = false;
+            this.updateTasksHash({ task: task, action: "delete" });
+          });
+      });
+    },
+    updateFacilities(updatedTask, id) {
+      var facilities = this.getUnfilteredFacilities;
+
+      facilities.forEach((facility) => {
+        if (facility.facilityProjectId === id) {
+          facility.tasks.push(updatedTask);
+        }
+      });
+    },
+    updateFacilityTask(task) {
+      var facilities = this.getUnfilteredFacilities;
+
+      var facilityIndex = facilities.findIndex(
+        (item) => item.facilityProjectId === task.facilityProjectId
+      );
+
+      facilities[facilityIndex].tasks.push(task);
+    },
+    createDuplicate() {
+      let url = `/projects/${this.currentProject.id}/facilities/${this.task.facilityId}/tasks/${this.task.id}/create_duplicate.json`;
+      let method = "POST";
+      let callback = "task-created";
+
+      let formData = new FormData();
+      formData.append("id", this.task.id);
+
+      axios({
+        method: method,
+        url: url,
+        data: formData,
+        headers: {
+          "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]')
+            .attributes["content"].value,
+        },
+      })
+        .then((response) => {
+          this.$emit(callback, humps.camelizeKeys(response.data.task));
+          this.updateFacilityTask(
+            humps.camelizeKeys(response.data.task),
+            this.task.facilityProjectId
+          );
+          if (response.status === 200) {
+            this.$message({
+              message: `${this.task.text} was duplicated successfully.`,
+              type: "success",
+              showClose: true,
+            });
+          }
+        })
+        .catch((err) => {
+          this.$message({
+            message: `Unable to duplicate ${this.task.text}. Please try again.`,
+            type: "error",
+            showClose: true,
+          });
+          // var errors = err.response.data.errors
+          console.log(err);
+        })
+        .finally(() => {
+          // this.loading = false
+        });
+    },
+    selectAllNodes() {
+      this.$refs.duplicatetree.setCheckedNodes(this.treeFormattedData);
+    },
+    clearAllNodes() {
+      this.$refs.duplicatetree.setCheckedNodes([]);
+    },
+    move(node) {
+      if (!node.hasOwnProperty("children")) {
+        this.moveTask(this.task, node.id);
+      }
+    },
+    duplicateSelectedTasks() {
+      this.submitted = true;
+
+      var facilityNodes = this.$refs.duplicatetree
+        .getCheckedNodes()
+        .filter((item) => !item.hasOwnProperty("children"));
+
+      var ids = facilityNodes.map((facility) => facility.id);
+
+      let url = `/projects/${this.currentProject.id}/facilities/${this.task.facilityId}/tasks/${this.task.id}/create_bulk_duplicate?`;
+      let method = "POST";
+      let callback = "task-created";
+
+      ids.forEach((id, index) => {
+        if (index === 0) {
+          url += `facility_project_ids[]=${id}`;
+        } else {
+          url += `&facility_project_ids[]=${id}`;
+        }
+      });
+
+      let formData = new FormData();
+      formData.append("id", this.task.id);
+      formData.append("facility_project_ids", ids);
+
+      axios({
+        method: method,
+        url: url,
+        data: formData,
+        headers: {
+          "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]')
+            .attributes["content"].value,
+        },
+      })
+        .then((response) => {
+          this.$emit(callback, humps.camelizeKeys(response.data.task));
+
+          response.data.tasks.forEach((task) => {
+            this.updateFacilityTask(
+              humps.camelizeKeys(task),
+              task.facilityProjectId
+            );
+          });
+          if (response.status === 200) {
+            this.$message({
+              message: `${this.task.text} was duplicated successfully to selected projects.`,
+              type: "success",
+              showClose: true,
+            });
+          }
+        })
+        .catch((err) => {
+          this.$message({
+            message: `Unable to duplicate ${this.task.text} to selected projects. Please try again.`,
+            type: "error",
+            showClose: true,
+          });
+          // var errors = err.response.data.errors
+          console.log(err);
+        })
+        .finally(() => {
+          // this.loading = false
+        });
+    },
+    filterNode(value, data) {
+      if (!value) return true;
+      return data.label.toLowerCase().indexOf(value.toLowerCase()) !== -1;
+    },
+    deleteTask() {
+      let confirm = window.confirm(
+        `Are you sure you want to delete "${this.task.text}"?`
+      );
+      if (!confirm) {
+        return;
+      }
+      this.taskDeleted(this.task);
+    },
+    toggleSubmitBtn() {
+      this.submitted = false;
+    },
+  },
+  watch: {
+    filterTree(value) {
+      this.$refs.duplicatetree.filter(value);
+      this.$refs.movetree.filter(value);
+    },
   },
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
 .context-menu {
   position: fixed;
   background: white;
@@ -70,5 +419,38 @@ export default {
   outline: none;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   cursor: pointer;
+}
+hr {
+  margin: 0;
+}
+.el-menu-item {
+  padding: 10px;
+  line-height: unset;
+  height: unset;
+  text-align: center;
+  overflow-x: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  &:hover {
+    background-color: rgba(91, 192, 222, 0.3);
+  }
+}
+.context-menu-btns,
+.filter-input {
+  padding: 10px;
+}
+.el-menu-item {
+  padding-right: 30px;
+}
+.el-tree {
+  padding: 10px;
+  max-width: 300px;
+  max-height: 300px;
+  overflow-y: auto;
+}
+.menu-subwindow-title {
+  font-size: 14px;
+  text-align: center;
+  margin-top: 10px;
 }
 </style>
