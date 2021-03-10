5050df74e9c9ef50417db84f9215cafd37ff6ceb
bilel-kedidi <bilel.kedidi@gmail.com>
2021-03-10 23:33:48 +0500

    Added test case for position sorting in users table

--- spec/cypress/integration/admin_panel/dashboard_spec.js
+++ spec/cypress/integration/admin_panel/dashboard_spec.js
@@ -8,6 +8,7 @@ describe('Admin Panel Dashboard', function() {
   it('Click on Admin Panel redirect to admin dashboard', function() {
     cy.get('[data-cy=admin_panel]').click()
     cy.contains('Dashboard')
+    cy.get('.blank_slate').contains('Welcome to Active Admin. This is the default dashboard page.')
     cy.get('#logout').click()
   })
 })
