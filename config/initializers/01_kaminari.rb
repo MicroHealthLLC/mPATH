509d72d7f4e42a0d1a5272355e09103e97a2d06e
root <root@HP-ENVY-188.localdomain>
2021-03-11 17:22:39 -0500

    Hotfix for bo

--- config/initializers/01_kaminari.rb
+++ config/initializers/01_kaminari.rb
@@ -1,4 +1,4 @@
-# For this file name(s): https://github.com/activeadmin/activeadmin/issues/4839#issuecomment-333132576
+# For this file name: https://github.com/activeadmin/activeadmin/issues/4839#issuecomment-333132576
 Kaminari.configure do |config|
   config.page_method_name = :per_page_kaminari
 end
