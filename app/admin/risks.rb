a67c5573c386bebe7e32f122c32e6e2a05b552ac
Amand Singh <amand.singh@microhealthllc.com>
2021-03-12 16:28:34 -0500

    fix error

--- app/admin/risks.rb
+++ app/admin/risks.rb
@@ -78,8 +78,14 @@ ActiveAdmin.register Risk do
     end
     column "Files" do |risk|
       risk.risk_files.map do |file|
+
+        next if file.nil? || !file.blob.filename.instance_variable_get("@filename").present?
         if current_user.admin_write?
-          link_to "#{file.blob.filename}", "#{Rails.application.routes.url_helpers.rails_blob_path(file, only_path: true)}", target: '_blank'
+          if file.blob.content_type == "text/plain"
+            link_to file.blob.filename.instance_variable_get("@filename"), file.blob.filename.instance_variable_get("@filename"), target: '_blank'
+          else
+            link_to "#{file.blob.filename}", "#{Rails.application.routes.url_helpers.rails_blob_path(file, only_path: true)}", target: '_blank'
+          end
         else
           "<span>#{file.blob.filename}</span>".html_safe
         end
--- app/admin/tasks.rb
+++ app/admin/tasks.rb
@@ -64,8 +64,13 @@ ActiveAdmin.register Task do
     column :description, sortable: false
     column "Files" do |task|
       task.task_files.map do |file|
+        next if file.nil? || !file.blob.filename.instance_variable_get("@filename").present?
         if current_user.admin_write?
-          link_to "#{file.blob.filename}", "#{Rails.application.routes.url_helpers.rails_blob_path(file, only_path: true)}", target: '_blank'
+          if file.blob.content_type == "text/plain"
+            link_to file.blob.filename.instance_variable_get("@filename"), file.blob.filename.instance_variable_get("@filename"), target: '_blank'
+          else
+            link_to "#{file.blob.filename}", "#{Rails.application.routes.url_helpers.rails_blob_path(file, only_path: true)}", target: '_blank'
+          end
         else
           "<span>#{file.blob.filename}</span>".html_safe
         end
