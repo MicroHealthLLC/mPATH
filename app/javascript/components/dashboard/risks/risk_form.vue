1412a4202d72d96438e9b4fdc533ff98bb2d859f
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2021-03-31 13:32:23 -0400

    Update risk_form.vue

--- app/javascript/components/dashboard/risks/risk_form.vue
+++ app/javascript/components/dashboard/risks/risk_form.vue
@@ -1641,11 +1641,10 @@
               <div class="col-6 mb-2 pl-4 links-col">                   
                
                  <div class="input-group mb-1">
-                    <div class="d-block mt-1">
+                    <div v-if="_isallowed('write')" class="d-block mt-1">
                     <label class="font-lg">Add link</label>
                     <span
-                      class="ml-2 clickable"
-                      v-if="_isallowed('write')"
+                      class="ml-2 clickable"                    
                       @click.prevent="addFilesInput"
                     >
                       <i class="fas fa-plus-circle"></i>
