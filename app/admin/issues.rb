07b3f82a637f5e45740304e016065c32a893f16d
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2021-04-05 10:07:56 -0400

    Update issues.rb

--- app/admin/issues.rb
+++ app/admin/issues.rb
@@ -167,7 +167,7 @@ ActiveAdmin.register Issue do
         end
       end
 
-      tab 'Files and Links' do
+      tab 'Files & Links' do
         f.inputs 'Basic Details' do
           div id: 'uploaded-task-files', 'data-files': "#{f.object.files_as_json}"
           f.input :issue_files
