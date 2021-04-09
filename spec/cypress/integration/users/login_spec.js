0978c35fdf340b4287a09d7d0609e7338accd608
b-solution <bilel.kedidi@gmail.com>
2021-04-09 18:14:39 +0500

    Fixed assigned user test cases in sheets page

--- spec/cypress/integration/users/login_spec.js
+++ spec/cypress/integration/users/login_spec.js
@@ -12,28 +12,30 @@ describe('Login', function() {
   it ('Test login functionality for admin', function() {
     cy.login('admin@test.com', 'T3$tAdmin')
     cy.contains('Welcome, Test1!')
+    cy.logout()
   })
 
   it ('Test login functionality for client', function() {
     cy.login('client@test.com', 'T3$tClient')
     cy.contains('Welcome, Test2!')
+    cy.logout()
   })
 
   it('Login with wrong email', function() {
     cy.visit('/')
-    cy.get('[data-cy=user_email]').type('client@exapmle.com').should('have.value', 'client@exapmle.com')
-    cy.get('[data-cy=user_password]').type('password').should('have.value', 'password')
-    cy.get('[data-cy=user_remember_me]').click()
-    cy.get('[data-cy=submit]').click()
+    cy.get('[data-cy=user_email]').type('client@exapmle.com', {force: true}).should('have.value', 'client@exapmle.com')
+    cy.get('[data-cy=user_password]').type('password', {force: true}).should('have.value', 'password')
+    cy.get('[data-cy=user_remember_me]').click({force: true})
+    cy.get('[data-cy=submit]').click({force: true})
     cy.contains('Invalid Email or password.')
   })
 
   it('Login with wrong password', function() {
     cy.visit('/')
-    cy.get('[data-cy=user_email]').type('admin@test.com').should('have.value', 'admin@test.com')
-    cy.get('[data-cy=user_password]').type('password').should('have.value', 'password')
-    cy.get('[data-cy=user_remember_me]').click()
-    cy.get('[data-cy=submit]').click()
+    cy.get('[data-cy=user_email]').type('admin@test.com', {force: true}).should('have.value', 'admin@test.com')
+    cy.get('[data-cy=user_password]').type('password', {force: true}).should('have.value', 'password')
+    cy.get('[data-cy=user_remember_me]').click({force: true})
+    cy.get('[data-cy=submit]').click({force: true})
     cy.contains('Invalid Email or password.')
   })
 
