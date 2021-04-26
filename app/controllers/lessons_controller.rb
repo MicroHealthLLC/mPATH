66159bd4dbeefc731eecf3e79e7c4b77a029ff73
Amand Singh <amand.singh@microhealthllc.com>
2021-04-26 16:25:35 -0400

    adding authorization

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,30 +1,37 @@
 class LessonsController < AuthenticatedController
 
   def index
+    authorize!(:read, Lesson.new(project_id: params[:project_id]))
     project = Project.find(params[:project_id])
     lessons = project.lessons.includes([{lesson_files_attachments: :blob}, {lesson_users: [:user] }, {lesson_projects: [{facility_project: :facility}] }, :users, :facilities, :facility_projects, :notes, :lesson_details, :task, :risk, :issue, :task_type, :issue_type, :project ])
     render json: {lessons: lessons.map(&:to_json)}, status: 200
   end
 
   def create
+    authorize!(:create, Lesson.new)
     lesson = Lesson.new.create_or_update_lession(params, current_user)
     render json: {lesson: lesson}, status: 200
 
   end
 
   def update
-    lesson = Lesson.find(params[:id]).create_or_update_lession(params, current_user)
+    lesson = Lesson.find(params[:id])
+    authorize!(:create, lesson)
+    lesson.create_or_update_lession(params, current_user)
     render json: {lesson: lesson.to_json}, status: 200
   end
 
 
   def show
     lesson = Lesson.find(params[:id])
+    authorize!(:read, lesson)
+    
     render json: {lesson: lesson.to_json}, status: 200
   end
 
   def destroy
     lesson = Lesson.find(params[:id])
+    authorize!(:destroy, lesson)
     if lesson.destroy
       render json: {lesson: lesson}, status: 200
     else
