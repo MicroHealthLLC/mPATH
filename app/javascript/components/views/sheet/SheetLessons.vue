164ebbc2a30c04172dc1f6ba7bd1b5e387a12355
Christopher Calderon <christopher.calderon@microhealthllc.com>
2021-05-14 10:31:04 -0700

    WIP: Sort functions implemented for table.

--- app/javascript/components/views/sheet/SheetLessons.vue
+++ app/javascript/components/views/sheet/SheetLessons.vue
@@ -10,21 +10,21 @@
         <font-awesome-icon icon="plus-circle" />
         Add Lesson
       </button>
-      <table>
+      <table class="my-3">
         <colgroup></colgroup>
-        <tr>
-          <th>Lesson</th>
-          <th>Date</th>
-          <th>Successes</th>
-          <th>Failures</th>
-          <th>Best Practices</th>
+        <tr class="table-head">
+          <th @click="sortLessons('title')">Lesson</th>
+          <th @click="sortLessonsByDate">Date</th>
+          <th @click="sortLessons('description')">Description</th>
+          <th>Added By</th>
+          <th>Last Update</th>
         </tr>
-        <tr>
-          <td>Example Lesson Name</td>
-          <td>Today</td>
-          <td>Got everything done</td>
-          <td>Nothing</td>
-          <td>A best practice here...</td>
+        <tr v-for="lesson in lessons" :key="lesson.id" @click="openLesson">
+          <td>{{ lesson.title }}</td>
+          <td>{{ lesson.date }}</td>
+          <td>{{ lesson.description }}</td>
+          <td>Someone</td>
+          <td>I am the last update</td>
         </tr>
       </table>
     </div>
@@ -32,12 +32,144 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      lessons: [
+        {
+          id: 1,
+          title: "Z Lesson 1",
+          description:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, ipsa.",
+          date: "5/12/2021",
+          stage: "Stage 1",
+          assign_users: [],
+          successes: [],
+          failures: [],
+          best_practices: [],
+          files: [],
+          links: [],
+          update: [],
+        },
+        {
+          id: 2,
+          title: "Ultra Lesson 2",
+          description: "Amet consectetur, adipisicing elit. Quae, ipsa.",
+          date: "5/5/2021",
+          stage: "Stage 2",
+          assign_users: [],
+          successes: [],
+          failures: [],
+          best_practices: [],
+          files: [],
+          links: [],
+          update: [],
+        },
+        {
+          id: 3,
+          title: "Super Lesson 3",
+          description: "Dues adipisicing elit. Quae, ipsa.",
+          date: "11/20/2026",
+          stage: "Stage 2",
+          assign_users: [],
+          successes: [],
+          failures: [],
+          best_practices: [],
+          files: [],
+          links: [],
+          update: [],
+        },
+      ],
+      activeSortValue: "",
+      sortAsc: false,
+    };
+  },
+  methods: {
+    openLesson() {
+      console.log("OPENING LESSON");
+    },
+    sortLessons(value) {
+      // Determine whether to sort lessons ascending or descending
+      if (this.activeSortValue !== value || !this.sortAsc) {
+        this.sortAsc = true;
+      } else {
+        this.sortAsc = false;
+      }
+
+      // Sort ascending
+      if (this.sortAsc) {
+        console.log(`Sorting by ${value}: Ascending`);
+
+        this.lessons = this.lessons.sort((lesson1, lesson2) => {
+          if (lesson1[value].toUpperCase() < lesson2[value].toUpperCase()) {
+            return -1;
+          } else if (
+            lesson1[value].toUpperCase() > lesson2[value].toUpperCase()
+          ) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+        // Sort descending
+      } else {
+        console.log(`Sorting by ${value}: Descending`);
+
+        this.lessons = this.lessons.sort((lesson1, lesson2) => {
+          if (lesson1[value].toUpperCase() < lesson2[value].toUpperCase()) {
+            return 1;
+          } else if (
+            lesson1[value].toUpperCase() > lesson2[value].toUpperCase()
+          ) {
+            return -1;
+          } else {
+            return 0;
+          }
+        });
+      }
+      // Store active sort value
+      this.activeSortValue = value;
+    },
+    sortLessonsByDate() {
+      // Determine whether to sort lessons ascending or descending
+      if (this.activeSortValue !== "date" || !this.sortAsc) {
+        this.sortAsc = true;
+      } else {
+        this.sortAsc = false;
+      }
+      // Sort ascending
+      if (this.sortAsc) {
+        this.lessons = this.lessons.sort(
+          (lesson1, lesson2) => new Date(lesson2.date) - new Date(lesson1.date)
+        );
+        console.log("Sorting by date: Ascending");
+        // Sort descending
+      } else {
+        this.lessons = this.lessons.sort(
+          (lesson1, lesson2) => new Date(lesson1.date) - new Date(lesson2.date)
+        );
+        console.log("Sorting by date: Descending");
+      }
+      // Store active sort value
+      this.activeSortValue = "date";
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .wrapper {
   box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  border: 1px solid #dee2e6;
+}
+.table-head {
+  background-color: #ededed;
+  text-align: center;
+  th {
+    border: 1px solid #dee2e6;
+  }
+}
+td {
   border: 1px solid #dee2e6;
 }
 </style>
