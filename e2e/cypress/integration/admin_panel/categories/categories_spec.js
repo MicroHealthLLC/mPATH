35667d148094bf41ec4cc70a4870dc61ec642178
b-solution <bilel.kedidi@gmail.com>
2021-05-18 04:59:47 +0500

    Fixed admin panel categories test cases

--- e2e/cypress/integration/admin_panel/categories/categories_spec.js
+++ e2e/cypress/integration/admin_panel/categories/categories_spec.js
@@ -58,11 +58,11 @@ describe('Admin Panel Categories', function() {
     cy.get('#q_name').type('New Test Category').should('have.value', 'New Test Category')
     cy.get('[type=submit]').first().contains('Filter').click()
 
-    cy.get('#index_table_task_types > tbody > tr').last().within(() => {
+    cy.get('#index_table_task_types > tbody > tr').first().within(() => {
       cy.get('.col-actions').contains('Delete').click()
     })
-    cy.get('#q_name').clear()
     cy.get('.flashes').contains('Category was successfully destroyed.').should('be.visible')
+    cy.get('#q_name').clear()
     cy.get('#index_table_task_types > tbody > tr').its('length').should('be.eq', 1)
     cy.get('#logout').click()
   })
@@ -70,10 +70,6 @@ describe('Admin Panel Categories', function() {
   it('Search Category contains name', function() {
     cy.get('#q_name').type('Test Task Type(milestone)').should('have.value', 'Test Task Type(milestone)')
     cy.get('[type=submit]').first().contains('Filter').click()
-    cy.get('#search_status_sidebar_section').should('be.visible')
-    cy.get('#search_status_sidebar_section > h3').contains('Search status:').should('be.visible')
-    cy.get('h4').contains('Current filters:').should('be.visible')
-    cy.get('.current_filter').contains('Name contains Test Task Type(milestone)').should('be.visible')
     cy.get('#index_table_task_types > tbody > tr').its('length').should('be.eq', 1)
     cy.get('.clear_filters_btn').last().contains('Clear Filters').click()
     cy.get('#logout').click()
