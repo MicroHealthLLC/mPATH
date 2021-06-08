9d87684e9e5285ed7a6dc7a489d1ee40f7d7d4f6
Amand Singh <amand.singh@microhealthllc.com>
2021-06-08 17:42:08 -0400

    WIP: adding json response to signin

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,13 @@
-class SessionsController < Devise::SessionsController
-
+class Users::SessionsController < Devise::SessionsController
+  protect_from_forgery with: :exception, prepend: true,  unless: -> { request.format.json? }
   def create
     user = User.find_by_email(sign_in_params[:email])
 
     if user && user.valid_password?(sign_in_params[:password])
       @current_user = user
+      if request.format.json?
+        render json: { token: user.authentication_token }, status: :ok
+      end
     else
       render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
     end
@@ -12,21 +15,21 @@ class SessionsController < Devise::SessionsController
 
   private
 
-  def respond_with(resource, _opts = {})
-    render json: { message: 'You are logged in.' }, status: :ok
-  end
+  # def respond_with(resource, _opts = {})
+  #   render json: { message: 'You are logged in.' }, status: :ok
+  # end
 
-  def respond_to_on_destroy
-    log_out_success && return if current_user
+  # def respond_to_on_destroy
+  #   log_out_success && return if current_user
 
-    log_out_failure
-  end
+  #   log_out_failure
+  # end
 
-  def log_out_success
-    render json: { message: "You are logged out." }, status: :ok
-  end
+  # def log_out_success
+  #   render json: { message: "You are logged out." }, status: :ok
+  # end
 
-  def log_out_failure
-    render json: { message: "Hmm nothing happened."}, status: :unauthorized
-  end
+  # def log_out_failure
+  #   render json: { message: "Hmm nothing happened."}, status: :unauthorized
+  # end
 end
