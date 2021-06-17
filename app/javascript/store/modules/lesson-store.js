176038c471723004b5b73c04bc79021e607346b8
Christopher Calderon <christopher.calderon@microhealthllc.com>
2021-06-17 11:43:07 -0700

    WIP: Add form data code to new lesson request.

--- app/javascript/store/modules/lesson-store.js
+++ app/javascript/store/modules/lesson-store.js
@@ -73,11 +73,55 @@ const lessonModule = {
     addLesson({ commit }, { lesson, programId, projectId }) {
       commit("TOGGLE_LESSONS_LOADED", false);
       // Add new lesson
-      lesson = {
-        lesson: {
-          ...lesson,
-        },
-      };
+      let formData = new FormData();
+
+      formData.append("lesson[title]", lesson.title);
+      formData.append("lesson[description]", lesson.description);
+      formData.append("lesson[date]", lesson.date);
+      formData.append("lesson[task_type_id]", lesson.task_type_id);
+      formData.append("lesson[user_id]", lesson.user_id);
+      formData.append("lesson[lesson_stage_id]", lesson.lesson_stage_id);
+      formData.append("lesson[important]", lesson.important);
+      formData.append("lesson[reportable]", lesson.reportable);
+      formData.append("lesson[draft]", lesson.draft);
+      // Prep Related Tasks
+      lesson.sub_task_ids.forEach((id) => {
+        formData.append("lesson[sub_task_ids][]", id);
+      });
+      // Prep Related Issues
+      lesson.sub_issue_ids.forEach((id) => {
+        formData.append("lesson[sub_issue_ids][]", id);
+      });
+      // Prep Related Risks
+      lesson.sub_risk_ids.forEach((id) => {
+        formData.append("lesson[sub_risk_ids][]", id);
+      });
+      // Prep Successes
+      lesson.successes.forEach((success) => {
+        Object.entries(success).forEach(([key, value]) => {
+          formData.append(`lesson[successes][][${key}]`, value);
+        });
+      });
+      //Prep Failures
+      lesson.failures.forEach((failure) => {
+        // formData.append(`lesson[failures][]`, JSON.stringify(failure) );
+        Object.entries(failure).forEach(([key, value]) => {
+          formData.append(`lesson[failures][][${key}]`, value);
+        });
+      });
+      //Prep Best Practices
+      lesson.best_practices.forEach((bestPractice) => {
+        Object.entries(bestPractice).forEach(([key, value]) => {
+          formData.append(`lesson[best_practices][][${key}]`, value);
+        });
+      });
+      //Prep Updates
+      lesson.notes_attributes.forEach((update) => {
+        Object.entries(update).forEach(([key, value]) => {
+          formData.append(`lesson[notes_attributes][][${key}]`, value);
+        });
+      });
+      
       axios({
         method: "POST",
         url: `/api/v1/programs/${programId}/projects/${projectId}/lessons.json`,
@@ -125,26 +169,26 @@ const lessonModule = {
         formData.append("lesson[sub_risk_ids][]", id);
       });
       // Prep Successes
-      lesson.successes.forEach((success, index) => {
+      lesson.successes.forEach((success) => {
         Object.entries(success).forEach(([key, value]) => {
           formData.append(`lesson[successes][][${key}]`, value);
         });
       });
       //Prep Failures
-      lesson.failures.forEach((failure, index) => {
+      lesson.failures.forEach((failure) => {
         // formData.append(`lesson[failures][]`, JSON.stringify(failure) );
-        Object.entries(failure).forEach(([key, value]) => {        
+        Object.entries(failure).forEach(([key, value]) => {
           formData.append(`lesson[failures][][${key}]`, value);
         });
       });
       //Prep Best Practices
-      lesson.best_practices.forEach((bestPractice, index) => {
+      lesson.best_practices.forEach((bestPractice) => {
         Object.entries(bestPractice).forEach(([key, value]) => {
           formData.append(`lesson[best_practices][][${key}]`, value);
         });
       });
       //Prep Updates
-      lesson.notes_attributes.forEach((update, index) => {
+      lesson.notes_attributes.forEach((update) => {
         Object.entries(update).forEach(([key, value]) => {
           formData.append(`lesson[notes_attributes][][${key}]`, value);
         });
