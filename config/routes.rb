1b5d8f56ce28fac941b7f3c412531da25ff0e137
Amand Singh <amand.singh@microhealthllc.com>
2021-07-12 16:18:55 -0400

    Adding lessons API

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
+      get "/portfolio/lessons", to: "portfolio#lessons"
       get "/portfolio/tasks", to: "portfolio#tasks"
       get "/portfolio/risks", to: "portfolio#risks"
       get "/portfolio/issues", to: "portfolio#issues"
