580ddf03d96fdb361b3e20e87056ace4de9ac885
Amand Singh <amand.singh@microhealthllc.com>
2021-07-15 13:07:29 -0400

    Setting log configuration to rotate log

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,6 +33,7 @@ Rails.application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+  config.logger = ActiveSupport::Logger.new(config.paths['log'].first, 2, 500.kilobytes)
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
