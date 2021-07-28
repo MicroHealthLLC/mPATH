d786497cd44bef837dbc69b4504d1104592de256
Amand Singh <amand.singh@microhealthllc.com>
2021-07-28 16:12:40 -0400

    Fix client side test cases

--- spec/cypress/integration/client_panel/admin_user/sheet/overview_specs.js
+++ spec/cypress/integration/client_panel/admin_user/sheet/overview_specs.js
@@ -9,26 +9,27 @@ describe('Sheet Overview Page', function() {
   it('Open Sheet overview to display the summary of a facility', function() {
     cy.get('[data-cy=facility_sheets]').within(() => {
       // cy.get('[data-cy=facility_tabs]').first().contains('Overview').should('be.visible')
-      cy.contains('Project Group:').should('be.visible')
-      cy.contains('Completion Date:').should('be.visible')
-      cy.contains('Status:').should('be.visible')
+      cy.contains('PROJECT GROUP:').should('be.visible')
+      cy.get('.mt-2').scrollIntoView()
+      cy.contains('COMPLETION DATE:').should('be.visible')
+      cy.contains('STATUS:').should('be.visible')
 
       // In overview display task summary
-      cy.get('[data-cy=facility_tasks]').scrollIntoView()
+      // cy.get('[data-cy=facility_tasks]').scrollIntoView()
       cy.get('[data-cy=facility_tasks]').within(() => {
         cy.contains('TASKS').should('be.visible')
         cy.contains('2').should('be.visible')
       })
 
       // In overview display Issues summary
-      cy.get('[data-cy=facility_issues]').scrollIntoView()
+      // cy.get('[data-cy=facility_issues]').scrollIntoView()
       cy.get('[data-cy=facility_issues]').within(() => {
         cy.contains('ISSUES').should('be.visible')
         cy.contains('2').should('be.visible')
       })
 
       // In overview display Issues summary
-      cy.get('[data-cy=facility_risks]').scrollIntoView()
+      // cy.get('[data-cy=facility_risks]').scrollIntoView()
       cy.get('[data-cy=facility_risks]').within(() => {
         cy.contains('RISKS').should('be.visible')
         cy.contains('2').should('be.visible')
