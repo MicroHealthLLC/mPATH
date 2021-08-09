e31ee000c4e1ae2e0c63d84d75bc9e82a0f745bf
Amand Singh <amand.singh@microhealthllc.com>
2021-08-09 17:11:08 -0400

    Improve specs

--- spec/cypress/integration/admin_panel/categories/categories_spec.js
+++ spec/cypress/integration/admin_panel/categories/categories_spec.js
@@ -1,23 +1,26 @@
 describe('Admin Panel Categories', function() {
-  beforeEach(() => {
+  before(() => {
+    cy.clearCookies()
     cy.app('clean')
     cy.appScenario('basic')
     cy.login('admin@test.com', 'T3$tAdmin')
     cy.openCategoryAP()
   })
-
+  beforeEach(() => {
+    cy.preserveAllCookiesOnce()
+  })
   it('Click on Categories on tabs open Category information page', function() {
     cy.get('#page_title').contains('Categories').should('be.visible')
     cy.get('#index_table_task_types').should('be.visible')
     cy.get('#index_table_task_types > tbody > tr').its('length').should('be.eq', 1)
-    cy.get('#logout').click()
+    // cy.get('#logout').click()
   })
 
   it('Open and close new Category form', function() {
     cy.get('.action_item > a').contains('New Category').click()
     cy.get('#page_title').contains('New Category').should('be.visible')
     cy.get('.cancel > a').contains('Cancel').click()
-    cy.get('#logout').click()
+    // cy.get('#logout').click()
   })
 
   it('Create new Category', function() {
@@ -27,7 +30,7 @@ describe('Admin Panel Categories', function() {
     cy.get('#task_type_submit_action').contains('Create Category').click()
     cy.get('.flashes').contains('Category was successfully created.')
     cy.get('#index_table_task_types > tbody > tr').its('length').should('be.eq', 2)
-    cy.get('#logout').click()
+    // cy.get('#logout').click()
   })
 
   it('Could not create new Category if name is blank', function() {
@@ -37,25 +40,31 @@ describe('Admin Panel Categories', function() {
     cy.get('.errors').contains("Name can't be blank")
     cy.get('.inline-errors').contains("can't be blank")
     cy.get('#page_title').contains('New Category').should('be.visible')
-    cy.get('#logout').click()
+    // cy.get('#logout').click()
   })
 
   it('Could not Delete Category of foreign constraint', function() {
+    cy.get('#tabs').within(() => {
+      cy.get('#task_types').contains('Categories').click({force: true})
+    })
     cy.get('#index_table_task_types').should('be.visible')
     cy.get('#index_table_task_types > tbody > tr').first().within(() => {
       cy.get('.col-actions').contains('Delete').click()
     })
     cy.get('.flashes').contains('Not able to delete this! Violates foreign key constraint.').should('be.visible')
-    cy.get('#index_table_task_types > tbody > tr').its('length').should('be.eq', 1)
-    cy.get('#logout').click()
+    cy.get('#index_table_task_types > tbody > tr').its('length').should('be.eq', 2)
+    // cy.get('#logout').click()
   })
 
   it('Delete Category', function() {
+    cy.get('#tabs').within(() => {
+      cy.get('#task_types').contains('Categories').click({force: true})
+    })
     cy.get('.action_item > a').contains('New Category').click()
-    cy.get('#task_type_name').type('New Test Category').should('have.value', 'New Test Category')
+    cy.get('#task_type_name').type('New Test Category 1').should('have.value', 'New Test Category 1')
     cy.get('#task_type_submit_action').contains('Create Category').click()
 
-    cy.get('#q_name').type('New Test Category').should('have.value', 'New Test Category')
+    cy.get('#q_name').type('New Test Category 1').should('have.value', 'New Test Category 1')
     cy.get('[type=submit]').first().contains('Filter').click()
 
     cy.get('#index_table_task_types > tbody > tr').first().within(() => {
@@ -63,11 +72,14 @@ describe('Admin Panel Categories', function() {
     })
     cy.get('#q_name').clear()
     cy.get('.flashes').contains('Category was successfully destroyed.').should('be.visible')
-    cy.get('#index_table_task_types > tbody > tr').its('length').should('be.eq', 1)
-    cy.get('#logout').click()
+    cy.get('#index_table_task_types > tbody > tr').its('length').should('be.eq', 2)
+    // cy.get('#logout').click()
   })
 
   it('Search Category contains name', function() {
+    cy.get('#tabs').within(() => {
+      cy.get('#task_types').contains('Categories').click({force: true})
+    })
     cy.get('#q_name').type('Test Task Type(milestone)').should('have.value', 'Test Task Type(milestone)')
     cy.get('[type=submit]').first().contains('Filter').click()
     cy.get('#search_status_sidebar_section').should('be.visible')
@@ -76,6 +88,6 @@ describe('Admin Panel Categories', function() {
     cy.get('.current_filter').contains('Name contains Test Task Type(milestone)').should('be.visible')
     cy.get('#index_table_task_types > tbody > tr').its('length').should('be.eq', 1)
     cy.get('.clear_filters_btn').last().contains('Clear Filters').click()
-    cy.get('#logout').click()
+    // cy.get('#logout').click()
   })
 })
