89dd363508d66a2652995a3be29234923a375646
Amand Singh <amand.singh@microhealthllc.com>
2021-08-25 15:29:08 -0400

    Fix test case

--- app/models/project_user.rb
+++ app/models/project_user.rb
@@ -2,10 +2,10 @@ class ProjectUser < ApplicationRecord
   belongs_to :user
   belongs_to :project
   after_create :provide_program_privileges
-  # after_destroy :remove_program_privileges
+  after_destroy :remove_program_privileges
 
   def remove_program_privileges
-    self.user.remove_program_privileges(self.project_id)
+    self.user.remove_all_privileges(self.project_id)
   end
 
   def provide_program_privileges
