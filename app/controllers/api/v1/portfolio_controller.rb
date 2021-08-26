4bb6e54aaf7cf8f7dce307a51c5d6e4c703b5320
Amand Singh <amand.singh@microhealthllc.com>
2021-08-26 14:53:18 -0400

    Fix performance issue on portfolio viewer

--- app/controllers/api/v1/portfolio_controller.rb
+++ app/controllers/api/v1/portfolio_controller.rb
@@ -5,11 +5,11 @@ class Api::V1::PortfolioController < AuthenticatedController
 
   def tab_counts
     json_response = {tasks_count: 0, issues_count: 0, risks_count: 0, lessons_count: 0}
-    
-    json_response[:tasks_count] = Task.unscoped.joins(:facility_project).where("facility_projects.project_id" => authorized_program_ids).count
-    json_response[:issues_count] = Issue.unscoped.joins(:facility_project).where("facility_projects.project_id" => authorized_program_ids).count
-    json_response[:risks_count] = Risk.unscoped.joins(:facility_project).where("facility_projects.project_id" => authorized_program_ids).count
-    json_response[:lessons_count] =  Lesson.unscoped.joins(:facility_project).where("facility_projects.project_id" => authorized_program_ids).count
+    program_ids = authorized_program_ids
+    json_response[:tasks_count] = Task.unscoped.joins(:facility_project).where("facility_projects.project_id" => program_ids).count
+    json_response[:issues_count] = Issue.unscoped.joins(:facility_project).where("facility_projects.project_id" => program_ids).count
+    json_response[:risks_count] = Risk.unscoped.joins(:facility_project).where("facility_projects.project_id" => program_ids).count
+    json_response[:lessons_count] =  Lesson.unscoped.joins(:facility_project).where("facility_projects.project_id" => program_ids).count
 
     render json: json_response
   end
@@ -23,7 +23,8 @@ class Api::V1::PortfolioController < AuthenticatedController
       end
       render json: json_response
     else
-      all_resources = Lesson.unscoped.joins(:facility_project).includes(Lesson.lesson_preload_array).where("facility_projects.project_id" => authorized_program_ids )
+      program_ids = authorized_program_ids
+      all_resources = Lesson.unscoped.joins(:facility_project).includes(Lesson.lesson_preload_array).where("facility_projects.project_id" => program_ids )
       json_response = []
       all_resources.in_batches(of: 1000) do |resources|
         resources.find_each do |resource|
@@ -68,7 +69,8 @@ class Api::V1::PortfolioController < AuthenticatedController
       render json: json_response
 
     else
-      all_resources = Task.unscoped.joins(:facility_project).includes([{task_files_attachments: :blob}, :task_type, :task_users, {users: :organization}, :task_stage, {checklists: [:user, {progress_lists: :user} ] }, { notes: :user }, :related_tasks, :related_issues, :related_risks, :sub_tasks, :sub_issues, :sub_risks, :project, :facility, :facility_group, {facility_project: [:facility, :status]} ]).where("facility_projects.project_id" => authorized_program_ids)
+      program_ids = authorized_program_ids
+      all_resources = Task.unscoped.joins(:facility_project).includes([{task_files_attachments: :blob}, :task_type, :task_users, {users: :organization}, :task_stage, {checklists: [:user, {progress_lists: :user} ] }, { notes: :user }, :related_tasks, :related_issues, :related_risks, :sub_tasks, :sub_issues, :sub_risks, :project, :facility, :facility_group, {facility_project: [:facility, :status]} ]).where("facility_projects.project_id" => program_ids)
       json_response = []
 
       all_resources.in_batches(of: 1000) do |resources|
@@ -93,7 +95,8 @@ class Api::V1::PortfolioController < AuthenticatedController
       render json: json_response
 
     else
-      all_resources = Issue.unscoped.joins(:facility_project).includes([{issue_files_attachments: :blob}, :issue_type, :task_type, :issue_users, {users: :organization}, :issue_stage, {checklists: [:user, {progress_lists: :user} ] },  { notes: :user }, :related_tasks, :related_issues,:related_risks, :sub_tasks, :sub_issues, :sub_risks, :project, :facility, :facility_group, {facility_project: [:facility, :status]}, :issue_severity ]).where("facility_projects.project_id" => authorized_program_ids)
+      program_ids = authorized_program_ids
+      all_resources = Issue.unscoped.joins(:facility_project).includes([{issue_files_attachments: :blob}, :issue_type, :task_type, :issue_users, {users: :organization}, :issue_stage, {checklists: [:user, {progress_lists: :user} ] },  { notes: :user }, :related_tasks, :related_issues,:related_risks, :sub_tasks, :sub_issues, :sub_risks, :project, :facility, :facility_group, {facility_project: [:facility, :status]}, :issue_severity ]).where("facility_projects.project_id" => program_ids)
       json_response = []
       all_resources.in_batches(of: 1000) do |resources|
         resources.find_each do |resource|
@@ -117,7 +120,9 @@ class Api::V1::PortfolioController < AuthenticatedController
       end
       render json: json_response
     else
-      all_resources = Risk.unscoped.joins(:facility_project).includes([{risk_files_attachments: :blob}, :task_type, :risk_users, {users: :organization},:risk_stage, {checklists: [:user, {progress_lists: :user} ] },  { notes: :user }, :related_tasks, :related_issues,:related_risks, :sub_tasks, :sub_issues, :sub_risks, :project, :facility, :facility_group, {facility_project: [:facility, :status]} ]).where("facility_projects.project_id" => authorized_program_ids)
+      program_ids = authorized_program_ids
+
+      all_resources = Risk.unscoped.joins(:facility_project).includes([{risk_files_attachments: :blob}, :task_type, :risk_users, {users: :organization},:risk_stage, {checklists: [:user, {progress_lists: :user} ] },  { notes: :user }, :related_tasks, :related_issues,:related_risks, :sub_tasks, :sub_issues, :sub_risks, :project, :facility, :facility_group, {facility_project: [:facility, :status]} ]).where("facility_projects.project_id" => program_ids)
       json_response = []
       all_resources.in_batches(of: 1000) do |resources|
         resources.find_each do |resource|
