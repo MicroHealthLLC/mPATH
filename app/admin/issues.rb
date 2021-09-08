fa5d4f1e8dcdd4deb92e39f3f942f8f73b93b9b7
Vin Kim <vin.kim@microhealthllc.com>
2021-09-08 11:35:26 -0400

    Files & Links tab - Add Links: Include instruction: Input link, then Enter

--- app/admin/issues.rb
+++ app/admin/issues.rb
@@ -173,7 +173,7 @@ ActiveAdmin.register Issue do
           div id: 'uploaded-task-files', 'data-files': "#{f.object.files_as_json}"
           f.input :issue_files
           div id: 'uploaded-task-links', 'data-links': "#{f.object.links_as_json}"
-          f.input :file_links, label: 'Add Links'
+          f.input :file_links, label: 'Add Links', hint: 'Input link, then "Enter"'
         end
       end
 
