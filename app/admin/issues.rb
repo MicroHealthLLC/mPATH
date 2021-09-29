d3f3abddbfb563ba1bf021abbde5bb9969784e1e
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2021-09-29 12:19:32 -0400

    Update issues.rb

--- app/admin/issues.rb
+++ app/admin/issues.rb
@@ -173,7 +173,7 @@ ActiveAdmin.register Issue do
           div id: 'uploaded-task-files', 'data-files': "#{f.object.files_as_json}"
           f.input :issue_files
           div id: 'uploaded-task-links', 'data-links': "#{f.object.links_as_json}"
-          f.input :file_links, label: 'Add Links', placeholder: 'Input link, then "Enter"'
+          f.input :file_links, label: 'Add Links', :attr, placeholder: 'Input link, then "Enter"'
         end
       end
 
