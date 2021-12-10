4066eb013b423b6a045c89f5d29c52aceb5076c3
Vin Kim <vin.kim@microhealthllc.com>
2021-12-10 11:11:50 -0500

    Sheet: Missing flags for Task, Risk, Issue

--- app/javascript/components/dashboard/issues/issue_sheets_index.vue
+++ app/javascript/components/dashboard/issues/issue_sheets_index.vue
@@ -466,6 +466,9 @@
             <span v-if="issue.important == true">Important</span>
             <span v-if="issue.isOverdue">Overdue</span>
             <span v-if="issue.progress == 100">Completed</span>   
+            <span v-if="issue.inProgress == true">In Progress</span>
+            <span v-if="issue.planned == true">Planned</span>
+            <span v-if="issue.reportable == true">Briefings</span>
             <span v-if="issue.onHold == true">On Hold</span> 
             <span v-if="issue.draft == true">Draft</span>   
             <span v-if="
@@ -473,6 +476,9 @@
                   issue.isOverdue == false &&
                   issue.onHold == false &&  
                   issue.draft == false && 
+                  issue.reportable == false &&
+                  issue.inProgress == false &&
+                  issue.planned == false &&
                   issue.progress < 100 "             
             >                 
             </span>  
