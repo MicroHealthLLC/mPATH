78c97fe346069e54111eb06b247ab161499e24f5
Vin Kim <vin.kim@microhealthllc.com>
2022-01-07 13:25:32 -0500

    Admin Panel - Should be Current Contract PoPs

--- app/admin/contract_current_pops.rb
+++ app/admin/contract_current_pops.rb
@@ -1,6 +1,8 @@
 ActiveAdmin.register ContractCurrentPop do
-  menu parent: "Contracts", priority: 1
+  menu parent: "Contracts", priority: 1, label: "Contract Current PoPs"
   actions :all, except: [:show]
+
+  index title: "Contract Current PoPs"
 
   permit_params do
     permitted = [:name]
