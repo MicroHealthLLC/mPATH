d77113779f080877d89ab0143a948d1091b82493
Amand Singh <amand.singh@microhealthllc.com>
2022-01-11 17:57:00 -0500

    Fix admin panel issue

--- app/admin/contract_current_pops.rb
+++ app/admin/contract_current_pops.rb
@@ -2,6 +2,14 @@ ActiveAdmin.register ContractCurrentPop do
   menu parent: "Contracts", priority: 1
   actions :all, except: [:show]
 
+  breadcrumb do
+    links = [link_to('Admin', admin_root_path), link_to('Contract Current Pop', admin_contract_current_pops_path)]
+    if %(show edit).include?(params['action'])
+      links << link_to(contract_current_pop.name, edit_admin_contract_current_pop_path)
+    end
+    links
+  end
+
   permit_params do
     permitted = [:name]
     permitted
--- app/admin/contract_vehicle_number.rb
+++ app/admin/contract_vehicle_number.rb
@@ -2,6 +2,14 @@ ActiveAdmin.register ContractVehicleNumber do
   menu parent: "Contracts", priority: 1
   actions :all, except: [:show]
 
+  breadcrumb do
+    links = [link_to('Admin', admin_root_path), link_to('Contract Vehicle Number', admin_contract_vehicle_numbers_path)]
+    if %(show edit).include?(params['action'])
+      links << link_to(contract_vehicle_number.name, edit_admin_contract_vehicle_number_path)
+    end
+    links
+  end
+
   permit_params do
     permitted = [:name]
     permitted
