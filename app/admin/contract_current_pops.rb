ed06d7671504cf086e3e7340b37c543f20f4410a
Vin Kim <vin.kim@microhealthllc.com>
2022-01-13 12:25:34 -0500

    Rename pop to PoP

--- app/admin/contract_current_pops.rb
+++ app/admin/contract_current_pops.rb
@@ -2,8 +2,12 @@ ActiveAdmin.register ContractCurrentPop do
   menu parent: "Contracts", priority: 1, label: "Contract Current PoPs"
   actions :all, except: [:show]
 
+  config.action_items[0] = ActiveAdmin::ActionItem.new only: :index do
+    link_to "New Contract Current PoP", new_admin_contract_current_pop_path
+  end
+
   breadcrumb do
-    links = [link_to('Admin', admin_root_path), link_to('Contract Current Pop', admin_contract_current_pops_path)]
+    links = [link_to('Admin', admin_root_path), link_to('Contract Current PoP', admin_contract_current_pops_path)]
     if %(show edit).include?(params['action'])
       links << link_to(contract_current_pop.name, edit_admin_contract_current_pop_path)
     end
@@ -19,6 +23,12 @@ ActiveAdmin.register ContractCurrentPop do
   controller do
     def edit
       @page_title = "Edit Contract Current PoP"
+      super
+    end
+
+    def new
+      @page_title = "New Contract Current PoP"
+      super
     end
   end
 end
