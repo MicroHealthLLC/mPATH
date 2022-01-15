ea7d03fe79b86735b3aec08c837253038444d619
Vin Kim <vin.kim@microhealthllc.com>
2022-01-15 12:58:23 -0500

    Fix edit contract category path

--- app/admin/contract_categories.rb
+++ app/admin/contract_categories.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register ContractCategory do
   breadcrumb do
     links = [link_to('Admin', admin_root_path), link_to('Contract Categories', admin_contract_categories_path)]
     if %(show edit).include?(params['action'])
-      links << link_to(contract_category.name, edit_admin_contract_categories_path)
+      links << link_to(contract_category.name, edit_admin_contract_category_path)
     end
     links
   end
