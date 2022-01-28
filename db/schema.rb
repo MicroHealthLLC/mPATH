f4e9c7006503d783f3ad79a0e526d3abf81cc753
Amand Singh <amand.singh@microhealthllc.com>
2022-01-28 13:39:43 -0500

    Revert change to remove project_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_28_161515) do
+ActiveRecord::Schema.define(version: 2022_01_26_220235) do
 
   create_table "active_admin_comments", charset: "utf8", force: :cascade do |t|
     t.string "namespace"
@@ -228,7 +228,6 @@ ActiveRecord::Schema.define(version: 2022_01_28_161515) do
     t.string "lng"
     t.integer "status", default: 1
     t.string "country_code", default: ""
-    t.integer "project_facility_group_id"
     t.index ["creator_id"], name: "index_facilities_on_creator_id"
     t.index ["facility_group_id"], name: "index_facilities_on_facility_group_id"
     t.index ["status"], name: "index_facilities_on_status"
@@ -243,6 +242,7 @@ ActiveRecord::Schema.define(version: 2022_01_28_161515) do
     t.integer "region_type", default: 0
     t.string "center"
     t.integer "progress", default: 0
+    t.integer "project_id"
   end
 
   create_table "facility_privileges", charset: "utf8", force: :cascade do |t|
