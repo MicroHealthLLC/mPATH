a0d7331b93c8a23afa33d58ffb131b371e7cd8ca
Amand Singh <amand.singh@microhealthllc.com>
2022-02-25 16:38:11 -0500

    Adding api to update privileges

--- app/controllers/api/v1/privileges_controller.rb
+++ app/controllers/api/v1/privileges_controller.rb
@@ -1,8 +1,17 @@
 class Api::V1::PrivilegesController < AuthenticatedController
   def get_privileges
-    pph = current_user.project_privileges_hash
-    fph = current_user.facility_privileges_hash
-    cph = current_user.contract_privileges_hash || {}
+    pph = current_user.project_privileges
+    fph = current_user.facility_privileges
+    cph = current_user.contract_privileges || {}
     render json: {program_privilges: pph, project_privileges: fph, contract_privileges: cph}
+  end
+
+  def update
+    current_user.update(params_permit)
+  end
+
+  private
+  def params_permit
+    params.require(:user).permit!
   end
 end
