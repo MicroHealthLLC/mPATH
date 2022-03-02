41543fa5290c1ae2375feae7ff3ea49a5219226a
Amand Singh <amand.singh@microhealthllc.com>
2022-03-02 16:43:26 -0500

    hide and show link with privileges in contract

--- app/admin/contract_vehicles.rb
+++ app/admin/contract_vehicles.rb
@@ -11,6 +11,16 @@ ActiveAdmin.register ContractVehicle do
 
     links
   end
+
+  index do
+    div id: '__privileges', 'data-privilege': "#{current_user.admin_privilege}"
+    column :name
+    actions defaults: false do |cc|
+      item "Edit", edit_admin_contract_vehicle_path(cc), title: 'Edit', class: "member_link edit_link" if current_user.admin_write?
+      item "Delete", admin_contract_vehicle_path(cc), title: 'Delete', class: "member_link delete_link", 'data-confirm': 'Are you sure you want to delete this?', method: 'delete' if current_user.admin_delete?
+    end
+  end
+
   batch_action :destroy, if: proc {current_user.admin_delete?}, confirm: "Are you sure you want to delete these #{ContractVehicle.name.titleize}" do |ids|
     deleted = ContractVehicle.where(id: ids).destroy_all
     redirect_to collection_path, notice: "Successfully deleted #{deleted.count} #{ContractVehicle.name.titleize}"
