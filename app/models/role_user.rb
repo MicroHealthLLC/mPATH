1563e82d86b369ab479d002af2456abacfcab9cb
Amand Singh <amand.singh@microhealthllc.com>
2022-03-29 12:19:50 -0400

    Modify role resonse

--- app/models/role_user.rb
+++ app/models/role_user.rb
@@ -1,9 +1,18 @@
 class RoleUser < ApplicationRecord
   belongs_to :user
   belongs_to :role
+  belongs_to :facility
+  belongs_to :contract
+  belongs_to :project
 
   def to_json(options={})
     role_user = self
-    role_user.attributes.merge({user_full_name: role_user.user.full_name, role_name: role_user.role.name})
+    role_user.attributes.merge({
+      user_full_name: role_user.user.full_name, 
+      role_name: role_user.role.name,
+      facility_name: role_user.facility&.facility_name,
+      project_name: role_user.project&.name,
+      contract_name: role_user.contract&.name
+    })
   end
 end
