7bbcdf1dd239dc6aa91ad98832484f44fad9fc40
Amand Singh <amand.singh@microhealthllc.com>
2022-04-04 14:55:02 -0400

    Fix issue with program setting

--- app/models/role_privilege.rb
+++ app/models/role_privilege.rb
@@ -10,7 +10,7 @@ class RolePrivilege < ApplicationRecord
   PROJECT_PRIVILEGS_ROLE_TYPES = ["project_analytics", "project_tasks", "project_issues", "project_risks", "project_lessons", "project_notes"]
   CONTRACT_PRIVILEGS_ROLE_TYPES = ["contract_analytics", "contract_tasks", "contract_issues", "contract_risks", "contract_lessons", "contract_notes"]
   PROGRAM_ADMIN_PRIVILEGS_ROLE_TYPES = ["program_admin"]
-  PROGRAM_SETTINGS_ROLE_TYPES = ["program_setting_groups", "program_setting_users", "program_setting_projects", "program_setting_contracts" ]
+  PROGRAM_SETTINGS_ROLE_TYPES = ["program_setting_groups", "program_setting_users_roles", "program_setting_projects", "program_setting_contracts" ]
 
   def check_privileges
     role = self.role
