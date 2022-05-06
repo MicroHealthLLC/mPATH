00c4f870db45bca05b0a23a80c65dfe97b17927a
Amand Singh <amand.singh@microhealthllc.com>
2022-05-06 16:40:28 -0400

    Adding api to remove facility project

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
         resources :facilities do
           collection do
             put :bulk_projects_update
+            get :remove_facility_project
           end
         end
 
