504f333c6ec644e9109bc9c7791df5007fe45849
Amand Singh <amand.singh@microhealthllc.com>
2022-05-23 13:10:51 -0400

    Fix error

--- app/controllers/api/v1/contract_project_data_controller.rb
+++ app/controllers/api/v1/contract_project_data_controller.rb
@@ -22,5 +22,9 @@ class Api::V1::ContractProjectDataController < AuthenticatedController
       render json: {errors: contract_project_data.errors.full_message}, status: 406
     end
   end
-  
+    
+  private
+  def contract_project_data_params
+    params.require(:contract_project_data).permit(:name, :charge_code, :contract_customer_id, :contract_award_to_id, :contract_type_id, :prime_or_sub, :contract_start_date, :contract_end_date, :total_contract_value, :contract_pops, :contract_current_pop_id, :contract_current_pop_start_date, :contract_current_pop_end_date, :total_founded_value, :billings_to_date, :comments, :pm_contract_poc_id, :gov_contract_poc_id, :co_contract_poc_id, :contract_naic_id)
+  end
 end
