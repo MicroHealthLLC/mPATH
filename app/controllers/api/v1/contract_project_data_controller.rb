6cb879d5c88552c2a21ab89d6854d6e1f401e44e
Amand Singh <amand.singh@microhealthllc.com>
2022-05-24 18:16:09 -0400

    Adding authorization in API

--- app/controllers/api/v1/contract_project_data_controller.rb
+++ app/controllers/api/v1/contract_project_data_controller.rb
@@ -1,4 +1,17 @@
 class Api::V1::ContractProjectDataController < AuthenticatedController
+  before_action :check_contract_read_permission, only: [:index]
+  before_action :check_contract_write_permission, only: [:create, :update, :add_project]
+  before_action :check_contract_delete_permission, only: [:destroy]
+  
+  def check_contract_read_permission
+    raise CanCan::AccessDenied if !current_user.can_read_contract_data?
+  end
+  def check_contract_write_permission
+    raise CanCan::AccessDenied if !current_user.can_write_contract_data?
+  end
+  def check_contract_delete_permission
+    raise CanCan::AccessDenied if !current_user.can_delete_contract_data?
+  end
 
   def add_project
     contract_project_data = ContractProjectDatum.find(params[:id])
