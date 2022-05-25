a9dc9cc3b88f090d44b4e6a15e09f1bbfe34302d
Amand Singh <amand.singh@microhealthllc.com>
2022-05-25 17:01:53 -0400

    Adding contract api for program setting

--- app/controllers/api/v1/program_settings/contracts_controller.rb
+++ app/controllers/api/v1/program_settings/contracts_controller.rb
@@ -3,13 +3,22 @@ class Api::V1::ProgramSettings::ContractsController < AuthenticatedController
 
   def index
 
-    all_contracts = Contract.includes(:facility_group, :contract_facility_group).where(project_id: params[:project_id] )
+    # all_contracts = Contract.includes(:facility_group, :contract_facility_group).where(project_id: params[:project_id] )
 
+    # c = []
+    # all_contracts.in_batches do |contracts|
+    #   c << contracts.map(&:to_json)
+    # end if all_contracts.any?
+    # render json: {contracts: c, total_count: c.size}
+
+    contract_project_data_ids = ProjectContract.where(project_id: params[:projec_id]).pluck(:contract_project_data_id).compact.uniq
+    contract_project_datas = ContractProjectData.where(id: contract_project_data_ids )
     c = []
-    all_contracts.in_batches do |contracts|
-      c << contracts.map(&:to_json)
-    end if all_contracts.any?
+    contract_project_datas.in_batches do |contract_project_data|
+      c << contract_project_data.map(&:to_json)
+    end if contract_project_datas.any?
     render json: {contracts: c, total_count: c.size}
+
   end
 
 end
