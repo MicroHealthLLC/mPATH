dd66a4f35b1aef287005848972aa4aeffcb5b3f4
Amand Singh <amand.singh@microhealthllc.com>
2022-06-02 15:19:54 -0400

    Adding API to remove contract from project

--- app/controllers/api/v1/program_settings/contracts_controller.rb
+++ app/controllers/api/v1/program_settings/contracts_controller.rb
@@ -2,15 +2,6 @@ class Api::V1::ProgramSettings::ContractsController < AuthenticatedController
   before_action :check_program_admin
 
   def index
-
-    # all_contracts = Contract.includes(:facility_group, :contract_facility_group).where(project_id: params[:project_id] )
-
-    # c = []
-    # all_contracts.in_batches do |contracts|
-    #   c << contracts.map(&:to_json)
-    # end if all_contracts.any?
-    # render json: {contracts: c, total_count: c.size}
-
     contract_project_data_ids = ProjectContract.where(project_id: params[:project_id]).pluck(:contract_project_datum_id).compact.uniq
     contract_project_datas = ContractProjectDatum.where(id: contract_project_data_ids )
     c = []
@@ -19,6 +10,19 @@ class Api::V1::ProgramSettings::ContractsController < AuthenticatedController
     end if contract_project_datas.any?
     render json: {contracts: c, total_count: c.size}
 
+  end
+
+  def show
+    render json: {contract: ProjectContract.find(params[:id]).contract_project_datum.to_json}
+  end
+
+  def destroy
+    project_contarct = ProjectContract.find(params[:id])
+    if project_contarct.destroy
+      render json: {message: "Successfully removed association!"}
+    else
+      render json: {error: "Error removing association!"}
+    end
   end
 
 end
