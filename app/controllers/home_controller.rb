1ce30d581b1783e8fd23df1307593883d5b8c3ed
Amand Singh <amand.singh@microhealthllc.com>
2022-06-13 14:54:52 -0400

    Fix contract button show hide after login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -38,7 +38,7 @@ class HomeController < AuthenticatedController
   def landing
     # there is no need of eager loading here..
     @preferences = current_user.get_preferences
-    @active_projects = current_user.authorized_programs.includes([:facilities, :users, :tasks, :issues, :risks, :facility_projects ])
+    @active_projects = current_user.authorized_programs.includes([:facilities, :users, :tasks, :issues, :risks, :lessons, :facility_projects ])
   end
 
   def index
