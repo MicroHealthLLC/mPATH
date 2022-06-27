3d6972c3f6c6edb070d762cfb619b1ce24d86a1c
Amand Singh <amand.singh@microhealthllc.com>
2022-06-27 16:07:44 -0400

    Added validation

--- app/models/contract_project_poc.rb
+++ app/models/contract_project_poc.rb
@@ -2,9 +2,16 @@ class ContractProjectPoc < ApplicationRecord
   belongs_to :user
   before_save :filter_values
   validates :email,presence: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
-  validates :work_number,presence: true
-  validates :mobile_number, presence: true
+  validate :number_check
   def filter_values
     self.attributes.reject!{|k,v| v == 'undefined' }
   end
+
+  def number_check
+    if mobile_number.blank? && work_number.blank?
+      errors.add(:base, "Mobile number or work number is required")
+      return false
+    end
+  end
+
 end
