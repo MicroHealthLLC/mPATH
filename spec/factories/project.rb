d1ed92544eaa55c47d88fe93b61d7ef248eb7bbd
Amand Singh <amand.singh@microhealthllc.com>
2022-07-13 08:29:57 -0400

    Adding test case

--- spec/factories/project.rb
+++ spec/factories/project.rb
@@ -2,5 +2,22 @@ FactoryBot.define do
   factory :project do
     name    { Faker::Internet.email }
     description { Faker::Internet.password }
+    project_type_id { create(:project_type).id }
+    users {[create(:user)]}
   end
+  
+  factory :project_type do
+    name    { Faker::Internet.name }
+  end
+
+  factory :user do
+    first_name    { Faker::Name.first_name }
+    last_name    { Faker::Name.last_name }
+    email {Faker::Internet.email}
+    password {Faker::Internet.password}
+    password_confirmation {password}
+  end
+
+
+
 end
--- spec/requests/api/v1/projects_spec.rb
+++ spec/requests/api/v1/projects_spec.rb
@@ -3,7 +3,13 @@ require 'rails_helper'
 RSpec.describe "Api::V1::Projects", type: :request do
   describe "GET api/v1/programs/:id" do
     it "works! (now write some real specs)" do
-      project = FactoryBot.create(:project)
+      binding.pry
+      user = create(:user)
+      program_admin_role = Role.program_admin_user_role 
+      project = create(:project, admin_program_admins: [user.id.to_s])
+
+      program_admin_role.role_users.create(user_id: user.id, project_id: project.id)
+      
       get api_v1_projects_path(project.id)
       expect(response).to have_http_status(200)
     end
