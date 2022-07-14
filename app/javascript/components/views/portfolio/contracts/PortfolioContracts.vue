536fd32be7d0bf7fb34a7f0588ab5148ef5199cc
root <root@HP-ENVY-188.localdomain>
2022-07-14 16:20:14 -0400

    Removed sensitive data from puma file and fixed undefined bug in manage poc modal email field

--- app/javascript/components/views/portfolio/contracts/PortfolioContracts.vue
+++ app/javascript/components/views/portfolio/contracts/PortfolioContracts.vue
@@ -1423,6 +1423,7 @@ export default {
             });
 
         } else {      
+          console.log(contractPOCdata)
             this.createContractPOC({...contractPOCdata})
             }
        }     
--- config/puma.rb
+++ config/puma.rb
@@ -1,27 +1,23 @@
 max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 5) 
 min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
-
+environment ENV.fetch('RAILS_ENV')
 if Rails.env.production? 
-  workers Integer(ENV['WEB_CONCURRENCY'] || 2) 
-  threads min_threads_count, max_threads_count  
-  nakayoshi_fork 
-  wait_for_less_busy_worker 
-  fork_worker  
-  rails_port = ENV.fetch('PORT', 8443) 
-  environment rails_env_prod
-  pidfile ENV.fetch('PUMA_PIDFILE')  
-  ssl_bind( 
-    ENV.fetch('SSL_HOST'),
-    rails_port, 
-    key: ENV.fetch('SSL_KEY_FILE'), 
-    cert: ENV.fetch('SSL_CERT_FILE'), 
-    verify_mode: ENV.fetch('SSL_VERIFY_MODE')
-  ) 
+ workers Integer(ENV['WEB_CONCURRENCY'] || 2) 
+ threads min_threads_count, max_threads_count 
+ nakayoshi_fork 
+ wait_for_less_busy_worker 
+ fork_worker 
+ pidfile ENV.fetch('PUMA_PIDFILE') 
+ ssl_bind( 
+ ENV.fetch('SSL_HOST'),
+ ENV.fetch('PUMA_PORT'), 
+ key: ENV.fetch('SSL_KEY_FILE'), 
+ cert: ENV.fetch('SSL_CERT_FILE'), 
+ verify_mode: ENV.fetch('SSL_VERIFY_MODE')
+ ) 
 else 
-  threads max_threads_count, max_threads_count
-  port ENV.fetch("PORT") { 3000 }
-  environment ENV.fetch('RAILS_ENV')
-end  
-
-plugin :tmp_restart 
-preload_app! 
+ threads max_threads_count, max_threads_count
+ port ENV.fetch("PUMA_PORT") { 3000 }
+end 
+  plugin :tmp_restart 
+preload_app!
