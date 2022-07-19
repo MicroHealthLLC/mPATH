480387ac29c3dc2ef730226ce3e3b5ecc077812b
jrivera <juan.rivera@microhealthllc.com>
2022-07-19 16:43:53 -0400

    Revised puma file to support local and prod environments.

--- config/puma.rb
+++ config/puma.rb
@@ -1,45 +1,38 @@
-max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
+require 'yaml'
 
-min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
-
-environment ENV.fetch('RAILS_ENV') { 'development' }
-
-if ENV['RAILS_ENV'] == 'production'
-
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-
-threads min_threads_count, max_threads_count
-
-nakayoshi_fork
-
-wait_for_less_busy_worker
-
-fork_worker
-
-pidfile ENV.fetch('PUMA_PIDFILE')
-
-ssl_bind(
-
-ENV.fetch('SSL_HOST'),
-
-ENV.fetch('PUMA_PORT'),
-
-key: ENV.fetch('SSL_KEY_FILE'),
-
-cert: ENV.fetch('SSL_CERT_FILE'),
-
-verify_mode: ENV.fetch('SSL_VERIFY_MODE')
-
-)
-
-else
-
-threads max_threads_count, max_threads_count
-
-port ENV.fetch("PUMA_PORT") { 3000 }
-
+if File.exist?("./puma_config.yml")
+  yaml_data = YAML.load_file('./puma_config.yml')
+  yaml_data.each do |key, value|
+    next if !value
+    ENV[key] = value
+  end
 end
 
-plugin :tmp_restart
+rails_env = ENV.fetch('RAILS_ENV')
+puma_port = Integer(ENV.fetch('PUMA_PORT'))
+max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
+min_threads_count = ENV.fetch('RAILS_MIN_THREADS', max_threads_count)
 
+workers Integer(ENV.fetch('WEB_CONCURRENCY', 2))
+threads min_threads_count, max_threads_count
+nakayoshi_fork
+wait_for_less_busy_worker
+fork_worker
+environment rails_env
+
+if rails_env == 'production'
+  pidfile ENV.fetch('PUMA_PIDFILE')
+
+  ssl_bind(
+    ENV.fetch('SSL_HOST'),
+    puma_port,
+    key: ENV.fetch('SSL_KEY_FILE'),
+    cert: ENV.fetch('SSL_CERT_FILE'),
+    verify_mode: ENV.fetch('SSL_VERIFY_MODE')
+  )
+else
+port puma_port
+end
+plugin :tmp_restart
 preload_app!
+
