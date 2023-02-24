639703c0a9a06622004087c9b641b898daf90934
Amand Singh <amand.singh@microhealthllc.com>
2023-02-24 12:17:14 -0500

    Modify migration

--- db/migrate/20230223174135_change_task_column.rb
+++ db/migrate/20230223174135_change_task_column.rb
@@ -1,6 +1,10 @@
 class ChangeTaskColumn < ActiveRecord::Migration[6.1]
-  def change
-    change_column :tasks, :planned_effort, :decimal, default: 0.0, precision: 4, scale: 2
-    change_column :tasks, :actual_effort, :decimal, default: 0.0, precision: 4, scale: 2
+  def down
+    change_column :tasks, :planned_effort, :decimal, default: 0.0
+    change_column :tasks, :actual_effort, :decimal, default: 0.0
+  end
+  def up
+    change_column :tasks, :planned_effort, :decimal, default: 0.0, precision: 10, scale: 2
+    change_column :tasks, :actual_effort, :decimal, default: 0.0, precision: 10, scale: 2
   end
 end
