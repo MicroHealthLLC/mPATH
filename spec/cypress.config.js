3773358c4ac62b41b403a74f36fe8aa6ae652584
Amand Singh <amand.singh@microhealthllc.com>
2023-03-30 10:10:35 -0400

    WIP: Upgrade cypress

---
+++ spec/cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  defaultCommandTimeout: 10000,
+  pageLoadTimeout: 90000,
+  projectId: 'jrbzy4',
+  viewportWidth: 1366,
+  viewportHeight: 768,
+  video: false,
+  screenshotOnRunFailure: false,
+  watchForFileChanges: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+})
