74c09e96f2e9fcee235421df0db8503fa12e3a9c
Amand Singh <amand.singh@microhealthllc.com>
2023-06-02 17:54:18 -0400

    fix migration

--- db/migrate/20230523191817_create_efforts.rb
+++ db/migrate/20230523191817_create_efforts.rb
@@ -5,7 +5,7 @@ class CreateEfforts < ActiveRecord::Migration[7.0]
       t.decimal :hours, default: 0.0, precision: 4, scale: 2
       t.integer :user_id, null: false
       t.integer :resource_id, null: false
-      t.integer :resource_type, null: false
+      t.string :resource_type, null: false
       t.integer :facility_project_id, null: false
       t.boolean :projected, default: false
 
