4ae17f07692888c07eef398d808bea231c9f3b82
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2023-09-28 12:43:39 -0400

    Update notes_sheets_index.vue
    Fixes Issue #4918 Privs after reload. When user has privs on Notes, the Sorry message will not display after reload.

--- app/javascript/components/dashboard/notes/notes_sheets_index.vue
+++ app/javascript/components/dashboard/notes/notes_sheets_index.vue
@@ -70,10 +70,10 @@
             class="notes"
           ></notes-sheets>
         </div>      
-        <div v-show="filteredNotes.length <= 0" class="text-danger ml-3">No notes found..</div>
+        <div v-else class="text-danger ml-3">No notes found..</div>
         </div>
       <div v-else class="text-danger mx-2 mt-2">
-        <h5> <i>Sorry, you don't have read-permissions for this tab! Please click on any available tab.</i></h5>
+        <h5><i>Sorry, you don't have read-permissions for this tab! Please click on any available tab.</i></h5>
       </div>
     </div>
   </div>
