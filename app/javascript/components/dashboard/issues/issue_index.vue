d5e6879d877a9593ae08c86dc400fb070e1e8450
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2023-10-31 14:38:50 -0400

    Update issue_index.vue
    Search and select Process Area filter

--- app/javascript/components/dashboard/issues/issue_index.vue
+++ app/javascript/components/dashboard/issues/issue_index.vue
@@ -28,7 +28,8 @@
       value-key="id"
       multiple                                                                                                                                               
       clearable
-      placeholder="Select Process Area"
+      filterable
+      placeholder="Search and select Process Area"
       >
     <el-option 
       v-for="item in taskTypes"                                                     
