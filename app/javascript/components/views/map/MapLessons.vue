12242f0a587c5057753459561bed48a7b072161c
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2023-11-02 10:57:55 -0400

    Update MapLessons.vue
    Searchable Process Area Filter

--- app/javascript/components/views/map/MapLessons.vue
+++ app/javascript/components/views/map/MapLessons.vue
@@ -14,7 +14,8 @@
           value-key="id"
           multiple
           clearable
-          placeholder="Select Process Area"
+          filterable
+          placeholder="Search and select Process Area"
           >
           <el-option
             v-for="item in taskTypes"
