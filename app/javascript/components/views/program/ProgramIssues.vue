39bf447d0d87251d6ad4acfb63d1e01b5985ed7c
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2023-12-01 12:22:45 -0500

    Update ProgramIssues.vue
    Adjust MH Logo size

--- app/javascript/components/views/program/ProgramIssues.vue
+++ app/javascript/components/views/program/ProgramIssues.vue
@@ -59,7 +59,7 @@
             </div>
                 <div class="col-3 mt-3">
                 <img
-                    style="width: 145px"
+                    style="width: 150px; height: 21px;"
                   :src="require('../../../../assets/images/mh_logo.png')"
                 />
             </div>
