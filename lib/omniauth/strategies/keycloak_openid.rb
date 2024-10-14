0c8d08f84fda8bc22d253ae68d4bbfbe442600b0
root <root@HP-ENVY-188>
2024-10-13 22:23:54 -0400

    Revert omniauth keycloak strategy.

--- lib/omniauth/strategies/keycloak_openid.rb
+++ lib/omniauth/strategies/keycloak_openid.rb
@@ -9,7 +9,7 @@ module OmniAuth
         site:          "https://keycloak.microhealthllc.com",
         authorize_url: "/realms/master/protocol/openid-connect/auth",
         token_url:     "/realms/master/protocol/openid-connect/token",
-        user_info_url: "/realms/master/protocol/openid-connect/userinfo"
+        user_info_url: "/realms/master/protocol/openid-connect/userinfo",
         redirect_uri: "https://mpath-qa.microhealthllc/auth/keycloak/callback",
       }
 
