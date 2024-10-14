da7aa9fcc64cd92cc4d1d06ef8ecd159ab47743d
root <root@HP-ENVY-188>
2024-10-13 22:11:39 -0400

    Revert omniauth keycloak strategy.

--- lib/omniauth/strategies/keycloak_openid.rb
+++ lib/omniauth/strategies/keycloak_openid.rb
@@ -34,9 +34,9 @@ module OmniAuth
 
       private
 
-      def callback_url
-        options[:redirect_uri] || (full_host + script_name + callback_path)
-      end
+      # def callback_url
+      #   options[:redirect_uri] || (full_host + script_name + callback_path)
+      # end
     end
   end
 end
