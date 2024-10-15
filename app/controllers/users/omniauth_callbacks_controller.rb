e623ce151fc99511cefc32d246e10ef73e629466
bluenevus <frank.tucker@microhealthllc.com>
2024-10-15 00:34:30 -0400

    Update omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -18,13 +18,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def oauth2
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: "OAuth2") if is_navigational_format?
-    else
-      session["devise.oauth2_data"] = request.env["omniauth.auth"].except(:extra)
-      redirect_to new_user_registration_url
-    end
+      check_omniauth_auth
   end
   
   def okta
