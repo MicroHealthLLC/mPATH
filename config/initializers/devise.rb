f0051606d2c103c3c8a98bc3b5851afa680482c2
bluenevus <frank.tucker@microhealthllc.com>
2024-10-14 21:21:17 -0400

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,9 +273,9 @@ Devise.setup do |config|
       site: 'https://keycloak.microhealthllc.com',
       realm: 'master',
       base_url: 'https://keycloak.microhealthllc.com',
-      authorization_endpoint: 'https://keycloak.microhealthllc.com/realms/master/protocol/openid-connect/auth',
-      token_endpoint: 'https://keycloak.microhealthllc.com/realms/master/protocol/openid-connect/token',
-      userinfo_endpoint: 'https://keycloak.microhealthllc.com/realms/master/protocol/openid-connect/userinfo'
+      authorization_endpoint: '/realms/master/protocol/openid-connect/auth',
+      token_endpoint: '/realms/master/protocol/openid-connect/token',
+      userinfo_endpoint: '/realms/master/protocol/openid-connect/userinfo'
     },
     redirect_uri: "https://mpath-qa.microhealthllc.com/users/auth/keycloak_openid/callback",
     name: :keycloak_openid,
