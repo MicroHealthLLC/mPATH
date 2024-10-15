7dd5696de56b149da4a2b2c563cc66dbd3f105d0
bluenevus <frank.tucker@microhealthllc.com>
2024-10-14 23:12:33 -0400

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@ require Rails.root.join("lib", "omniauth", "strategies","office365.rb")
 require Rails.root.join("lib", "omniauth", "strategies","keycloak_openid.rb")
 require 'omniauth-okta'
 require 'omniauth-keycloak'
-# require 'omniauth-keycloak'
+require 'oauth2'
 
 # frozen_string_literal: true
 
