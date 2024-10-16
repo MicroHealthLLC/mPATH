52df79e8f3dffa38dedb903c900c9afe9c1584ba
bluenevus <frank.tucker@microhealthllc.com>
2024-10-15 21:51:25 -0400

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   
-  devise :omniauthable, omniauth_providers: [:office365, :google_oauth2], 
-         :database_authenticatable, :recoverable, :rememberable, :trackable,
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable,
+        # :omniauthable, omniauth_providers: [:office365, :google_oauth2], 
         # :validatable, :omniauthable, omniauth_providers: %i[google_oauth2 keycloak_openid office365 okta]
-        # :validatable, :omniauthable
+         :validatable, :omniauthable
 
   has_many :project_users, dependent: :destroy
   has_many :projects, through: :project_users
