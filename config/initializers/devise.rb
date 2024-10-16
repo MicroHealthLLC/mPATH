aec8318d6668531ed0944dd0d215ea78bbebdb10
bluenevus <frank.tucker@microhealthllc.com>
2024-10-16 00:34:14 -0400

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -267,7 +267,7 @@ config.omniauth :office365,
   ENV['OFFICE365_CLIENT_SECRET'],
   {
     scope: 'openid profile email',
-    fields: ['profile', 'email'],
+    # fields: ['profile', 'email'],
     client_options: {
       site: 'https://login.microsoftonline.com',
       authorize_url: '/common/oauth2/v2.0/authorize',
