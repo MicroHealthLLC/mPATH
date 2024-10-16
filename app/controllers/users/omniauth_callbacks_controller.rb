795b387a0d00a36cfac14dd8a55c35676adeae35
bluenevus <frank.tucker@microhealthllc.com>
2024-10-15 21:27:02 -0400

    Update omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,16 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def office365
-    check_omniauth_auth
+    # Handle the Office 365 callback
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: "Office 365") if is_navigational_format?
+    else
+      session["devise.office365_data"] = request.env["omniauth.auth"].except(:extra)
+      redirect_to new_user_registration_url
+    end
   end
 
   def google_oauth2
