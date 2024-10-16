d98665c3e0387d98080dfad2898bbf40efa2bd6b
bluenevus <frank.tucker@microhealthllc.com>
2024-10-15 22:27:36 -0400

    Update omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,15 +5,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
    
   def keycloak
-    @user = User.from_omniauth(request.env["omniauth.auth"])
 
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: "Keycloak") if is_navigational_format?
-    else
-      session["devise.keycloak_data"] = request.env["omniauth.auth"].except(:extra)
-      redirect_to new_user_registration_url
-    end
   end
 
   def failure
