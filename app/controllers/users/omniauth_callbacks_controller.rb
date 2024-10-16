7f58d44c2158870e4ee8f4585fbb3d6fda7ab27b
bluenevus <frank.tucker@microhealthllc.com>
2024-10-15 22:22:18 -0400

    Update omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,6 +3,22 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
    def office365
       # Handle Office 365 callback
     end
+   
+  def keycloak
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: "Keycloak") if is_navigational_format?
+    else
+      session["devise.keycloak_data"] = request.env["omniauth.auth"].except(:extra)
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def failure
+    redirect_to root_path
+  end
 
   def passthru
     Rails.logger.debug "Passthru path: #{request.fullpath}"
