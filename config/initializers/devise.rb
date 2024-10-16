ceb2d33563b1c0f236615610a1825173c35935dd
bluenevus <frank.tucker@microhealthllc.com>
2024-10-15 21:40:33 -0400

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -269,19 +269,17 @@ Devise.setup do |config|
 #  scope: 'openid email',
 #  redirect_uri: "https://mpath-qa.microhealthllc.com/auth/keycloak/callback"
 
-config.omniauth :office365,
-  ENV['OFFICE365_CLIENT_ID'],
-  ENV['OFFICE365_CLIENT_SECRET'],
-  {
-    scope: 'openid profile email offline_access https://graph.microsoft.com/User.Read https://graph.microsoft.com/Mail.Read',
-    client_options: {
-      site: 'https://graph.microsoft.com',
-      authorize_url: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
-      token_url: 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
-    },
-    provider_ignores_state: true,
-    prompt: 'select_account'
-  }
+  config.omniauth :office365,
+    ENV['OFFICE365_CLIENT_ID'],
+    ENV['OFFICE365_CLIENT_SECRET'],
+    {
+      scope: 'openid profile email offline_access https://graph.microsoft.com/User.Read',
+      client_options: {
+        site: 'https://login.microsoftonline.com',
+        authorize_url: '/common/oauth2/v2.0/authorize',
+        token_url: '/common/oauth2/v2.0/token'
+      }
+    }
     
 #   config.omniauth(:office365, 
 #    ENV['OFFICE365_KEY'], 
