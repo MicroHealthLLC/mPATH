389acc5e49fd5196a2a90427094482f2820a72f7
bluenevus <frank.tucker@microhealthllc.com>
2024-10-16 23:16:15 -0400

    Update omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,8 +1,20 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-   def office365
-      # Handle Office 365 callback
+  def office365
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: "Office 365") if is_navigational_format?
+    else
+      session["devise.office365_data"] = request.env["omniauth.auth"].except(:extra)
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
+  end
+
+  def failure
+    redirect_to root_path, alert: "Authentication failed, please try again."
+  end
 
   def failure
     redirect_to root_path
