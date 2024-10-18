0b4460c353f7f52eccd6f100aeb65a76823f094b
Zach Egner <zach.egner@microhealthllc.com>
2024-10-18 10:30:27 -0400

    fix o365 in dhs

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,7 +2,15 @@ class CallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :verify_authenticity_token
 
   def office365
-    check_omniauth_auth
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: "Office 365") if is_navigational_format?
+    else
+      session["devise.office365_data"] = request.env["omniauth.auth"].except(:extra)
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+    end
   end
 
   def google_oauth2
