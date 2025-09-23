383a44b8362b94706d048f25f485a0d5d611513e
Joseph Yousefpour <josephsor@gmail.com>
2025-09-23 16:25:02 -0400

    adding initial dockerfile for mPATH

--- config/puma.rb
+++ config/puma.rb
@@ -1,49 +1,33 @@
-require 'yaml'
+# frozen_string_literal: true
+require "yaml"
 
 # Load optional YAML config
-if File.exist?('./puma_config.yml')
-  yaml_data = YAML.load_file('./puma_config.yml')
-  yaml_data.each do |key, value|
-    ENV[key] = value if value
-  end
+if File.exist?("./puma_config.yml")
+  yaml_data = YAML.safe_load(File.read("./puma_config.yml")) || {}
+  yaml_data.each { |k, v| ENV[k.to_s] = v.to_s if v }
 end
 
-rails_env = ENV.fetch('RAILS_ENV')
-puma_port = Integer(ENV.fetch('PUMA_PORT'))
+rails_env = ENV.fetch("RAILS_ENV", "production")
+# Support either PUMA_PORT or PORT, fallback to 8443
+puma_port = Integer(ENV["PUMA_PORT"] || ENV["PORT"] || 8443)
 
-max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
-min_threads_count = ENV.fetch('RAILS_MIN_THREADS', max_threads_count)
+max_threads_count = Integer(ENV.fetch("RAILS_MAX_THREADS", 5))
+min_threads_count = Integer(ENV.fetch("RAILS_MIN_THREADS", max_threads_count))
 
-workers Integer(ENV.fetch('WEB_CONCURRENCY', 2))
+workers Integer(ENV.fetch("WEB_CONCURRENCY", 2))
 threads min_threads_count, max_threads_count
 
 environment rails_env
 
-# Optional logging for debug
-puts "[Puma] ENV PUMA_PORT=#{puma_port}"
-puts "[Puma] ENV RAILS_ENV=#{rails_env}"
 
-if rails_env == 'production'
-  pidfile ENV.fetch('PUMA_PIDFILE')
+STDOUT.puts "[Puma] ENV PORT=#{ENV['PORT']}"
+STDOUT.puts "[Puma] ENV PUMA_PORT=#{ENV['PUMA_PORT']}"
+STDOUT.puts "[Puma] RAILS_ENV=#{rails_env}"
 
-  # Commented out SSL bind since Nginx handles TLS
-  # begin
-  #   ssl_bind(
-  #     ENV.fetch('PUMA_SSL_HOST'),
-  #     puma_port,
-  #     key: ENV.fetch('PUMA_SSL_KEY_FILE'),
-  #     cert: ENV.fetch('PUMA_SSL_CERT_FILE'),
-  #     verify_mode: ENV.fetch('PUMA_SSL_VERIFY_MODE')
-  #   )
-  #   puts "[Puma] SSL bind successful on #{ENV['PUMA_SSL_HOST']}:#{puma_port}"
-  # rescue KeyError => e
-  #   warn "[Puma] Missing SSL ENV variable: #{e.message}"
-  #   exit 1
-  # end
+# PID file
+pidfile ENV.fetch("PUMA_PIDFILE", File.join(Dir.pwd, "tmp/pids/server.pid"))
 
-  port puma_port
-else
-  port puma_port
-end
+# We’re terminating TLS at ALB
+bind "tcp://0.0.0.0:#{puma_port}"
 
 plugin :tmp_restart
